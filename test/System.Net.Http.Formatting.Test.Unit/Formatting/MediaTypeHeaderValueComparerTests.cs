dd3e5c43930c65c905360c7c3fdbc809b280149e
henrikn <henrikn@microsoft.com>
2012-03-15 14:09:51 -0700

    Renamed misnamed file to MediaTypeHeaderValueComparerTests

--- test/System.Net.Http.Formatting.Test.Unit/Formatting/MediaTypeHeaderValueComparerTests.cs
+++ test/System.Net.Http.Formatting.Test.Unit/Formatting/MediaTypeHeaderValueComparerTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace System.Net.Http.Formatting
 {
-    public class MediaTypeHeadeValueComparerTests
+    public class MediaTypeHeaderValueComparerTests
     {
 
         [Fact]

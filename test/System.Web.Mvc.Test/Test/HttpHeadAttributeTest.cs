6e7594781f71bb3594a0d32f99dba025f156b400
Nick Berardi <nick@managedfusion.com>
2012-03-28 09:05:54 -0400

    Added HttpHeadAttribute to MVC as a fix for issue #5

---
+++ test/System.Web.Mvc.Test/Test/HttpHeadAttributeTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace System.Web.Mvc.Test
+{
+    public class HttpHeadAttributeTest
+    {
+        [Fact]
+        public void IsValidForRequestReturnsFalseIfHttpVerbIsNotHead()
+        {
+            HttpVerbAttributeHelper.TestHttpVerbAttributeWithInvalidVerb<HttpHeadAttribute>("GET");
+        }
+
+        [Fact]
+        public void IsValidForRequestReturnsTrueIfHttpVerbIsHead()
+        {
+            HttpVerbAttributeHelper.TestHttpVerbAttributeWithValidVerb<HttpHeadAttribute>("HEAD");
+        }
+
+        [Fact]
+        public void IsValidForRequestThrowsIfControllerContextIsNull()
+        {
+            HttpVerbAttributeHelper.TestHttpVerbAttributeNullControllerContext<HttpHeadAttribute>();
+        }
+    }
+}

f2081e0fbd1e9dc18581b94338af1455f73930c2
bradwilson <dotnetguy@gmail.com>
2012-04-13 14:29:15 -0700

    44: Updated XML docs for IDependencyScope

--- src/System.Web.Http/Dependencies/IDependencyScope.cs
+++ src/System.Web.Http/Dependencies/IDependencyScope.cs
@@ -12,18 +12,20 @@ namespace System.Web.Http.Dependencies
     public interface IDependencyScope : IDisposable
     {
         /// <summary>
-        /// Gets an instance of the given <paramref name="serviceType"/>. Must never throw.
+        /// Gets an instance of the given <paramref name="serviceType"/>. Must return <c>null</c>
+        /// if the service is not available (must not throw).
         /// </summary>
         /// <param name="serviceType">The object type.</param>
         /// <returns>The requested object, if found; <c>null</c> otherwise.</returns>
         object GetService(Type serviceType);
 
         /// <summary>
-        /// Gets all instances of the given <paramref name="serviceType"/>. Must never throw.
+        /// Gets all instances of the given <paramref name="serviceType"/>. Must return an empty
+        /// collection if the service is not available (must not return <c>null</c> or throw).
         /// </summary>
         /// <param name="serviceType">The object type.</param>
-        /// <returns>A sequence of isntances of the requested <paramref name="serviceType"/>. The sequence
-        /// should be empty (not null) if no objects of the given type are available.</returns>
+        /// <returns>A sequence of instances of the requested <paramref name="serviceType"/>. The sequence
+        /// should be empty (not <c>null</c>) if no objects of the given type are available.</returns>
         IEnumerable<object> GetServices(Type serviceType);
     }
 }

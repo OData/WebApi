5a2fc9edb66d120938191fce66b6ea7a1086c718
dotnetjunky <kinhln@microsoft.com>
2012-04-16 12:18:57 -0700

    Add unit test for the PreAppStartCode class.

--- test/Microsoft.Web.WebPages.OAuth.Test/PreAppStartCodeTest.cs
+++ test/Microsoft.Web.WebPages.OAuth.Test/PreAppStartCodeTest.cs
@@ -1,10 +1,17 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 
+using System.Web.WebPages.TestUtils;
+using Xunit;
+
 namespace Microsoft.Web.WebPages.OAuth.Test
 {
     public class PreAppStartCodeTest
     {
-
+        [Fact]
+        public void TestPreAppStartClass()
+        {
+            PreAppStartTestHelper.TestPreAppStartClass(typeof(PreApplicationStartCode));
+        }
     }
 }

5f36a21347b0fc04ae67df18b8c9e7936b655ace
marcind <marcind@microsoft.com>
2012-05-08 11:39:00 -0700

    Issue #81: Add more constructors to HttpResponseException
    Add ctor that takes in an HttpStatusCode

--- test/System.Web.Http.Test/HttpResponseExceptionTest.cs
+++ test/System.Web.Http.Test/HttpResponseExceptionTest.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
+using System.Net;
 using System.Net.Http;
 using Xunit;
 using Assert = Microsoft.TestCommon.AssertEx;
@@ -22,6 +23,21 @@ namespace System.Web.Http
             var exception = new HttpResponseException(response);
 
             Assert.Same(response, exception.Response);
+            if(Assert.CurrentCultureIsEnglish) {
+                Assert.Equal("Processing of the HTTP request resulted in an exception. Please see the HTTP response returned by the 'Response' property of this exception for details.", exception.Message);
+            }
+        }
+
+        [Fact]
+        public void Constructor_SetsResponsePropertyWithGivenStatusCode()
+        {
+            var exception = new HttpResponseException(HttpStatusCode.BadGateway);
+
+            Assert.Equal(HttpStatusCode.BadGateway, exception.Response.StatusCode);
+            if (Assert.CurrentCultureIsEnglish)
+            {
+                Assert.Equal("Processing of the HTTP request resulted in an exception. Please see the HTTP response returned by the 'Response' property of this exception for details.", exception.Message);
+            }
         }
     }
 }

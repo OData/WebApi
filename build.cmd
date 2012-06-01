c7c349dda0ae8382c23b6b5a79831930b1ea0f18
anurse <andrew@andrewnurse.net>
2012-06-01 10:52:21 -0700

    Added explicit Platform=Any CPU option to build.cmd to ease builds in x64 environments

--- build.cmd
+++ build.cmd
@@ -7,12 +7,12 @@ mkdir bin
 :Build
 if "%1" == "" goto BuildDefaults
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /t:%* /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /t:%* /p:Platform="Any CPU" /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if errorlevel 1 goto BuildFail
 goto BuildSuccess
 
 :BuildDefaults
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /p:Platform="Any CPU" /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if errorlevel 1 goto BuildFail
 goto BuildSuccess
 

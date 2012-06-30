c1bf28fb352dd0c8a919c115847f218672dc07d8
henrikn <henrikn@microsoft.com>
2012-06-30 15:21:45 -0700

    Added unit test for abstract MediaTypeMapping class.

---
+++ test/System.Net.Http.Formatting.Test.Unit/Formatting/MediaTypeMappingTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System.Net.Http.Headers;
+using Xunit;
+using Assert = Microsoft.TestCommon.AssertEx;
+
+namespace System.Net.Http.Formatting
+{
+    public class MediaTypeMappingTests
+    {
+        [Fact]
+        public void Constructor_ThrowsOnNullMediaTypeHeaderValue()
+        {
+            Assert.ThrowsArgumentNull(() => new MockMediaTypeMapping((MediaTypeHeaderValue)null), "mediaType");
+        }
+
+        [Fact]
+        public void Constructor_ThrowsOnNullMediaType()
+        {
+            Assert.ThrowsArgumentNull(() => new MockMediaTypeMapping((string)null), "mediaType");
+            Assert.ThrowsArgumentNull(() => new MockMediaTypeMapping(String.Empty), "mediaType");
+        }
+
+        public class MockMediaTypeMapping : MediaTypeMapping
+        {
+            public MockMediaTypeMapping(MediaTypeHeaderValue mediaType)
+                : base(mediaType)
+            {
+            }
+
+            public MockMediaTypeMapping(string mediaType)
+                : base(mediaType)
+            {
+            }
+
+            public override double TryMatchMediaType(HttpRequestMessage request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}

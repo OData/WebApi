bdfaf49f334ded1d4e9e460e751f52edc7f20c94
roncain <roncain@microsoft.com>
2012-07-11 08:33:06 -0700

    Merged System.Net.Http.Formatting unit and integration test projects into single project.

--- test/System.Net.Http.Formatting.Test/Properties/AssemblyInfo.cs
+++ test/System.Net.Http.Formatting.Test/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("System.Net.Http.Formatting.Test.Unit")]
+[assembly: AssemblyTitle("System.Net.Http.Formatting.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("System.Net.Http.Formatting.Test.Unit")]
+[assembly: AssemblyProduct("System.Net.Http.Formatting.Test")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,5 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: InternalsVisibleTo("System.Net.Http.Formatting.Test.Integration, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
 [assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "These assemblies are delay-signed.")]

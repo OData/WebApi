bf83bedb0d40bb7a446b90f705134bfcc3dbee57
henrikn <henrikn@microsoft.com>
2012-08-23 10:57:03 -0700

    Added HttpConfigurationExtensionsTest unit test

---
+++ test/System.Web.Http.Test/HttpConfigurationExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ModelBinding.Binders;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Net.Http
+{
+    public class HttpConfigurationExtensionsTest
+    {
+        [Fact]
+        public void BindParameter_GuardClauses()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            Type type = typeof(TestParameter);
+            IModelBinder binder = new Mock<IModelBinder>().Object;
+
+            Assert.ThrowsArgumentNull(() => HttpConfigurationExtensions.BindParameter(null, type, binder), "configuration");
+            Assert.ThrowsArgumentNull(() => HttpConfigurationExtensions.BindParameter(config, null, binder), "type");
+            Assert.ThrowsArgumentNull(() => HttpConfigurationExtensions.BindParameter(config, type, null), "binder");
+        }
+
+        [Fact]
+        public void BindParameter_InsertsModelBinderProviderInPositionZero()
+        {
+            // Arrange
+            HttpConfiguration config = new HttpConfiguration();
+            Type type = typeof(TestParameter);
+            IModelBinder binder = new Mock<IModelBinder>().Object;
+
+            // Act
+            config.BindParameter(type, binder);
+
+            // Assert
+            SimpleModelBinderProvider provider = config.Services.GetServices(typeof(ModelBinderProvider)).OfType<SimpleModelBinderProvider>().First();
+            Assert.Equal(type, provider.ModelType);
+        }
+
+        public class TestParameter
+        {
+        }
+    }
+}

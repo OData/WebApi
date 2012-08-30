8e271e81d98af549a4782943bbf05d032c31382b
Henrik Frystyk Nielsen <henrikn@microsoft.com>
2012-08-29 19:05:33 -0700

    Added GlobalSuppressions to System.Web.Http.SignalR project

---
+++ src/System.Web.Http.SignalR/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "System.Web.Http")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "This is untul SignalR gets signed.")]

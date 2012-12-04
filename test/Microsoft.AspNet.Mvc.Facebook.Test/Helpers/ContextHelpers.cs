d83cddeade5067b37a37dee947df1cf76f9e268f
yaohuang <yaohuang@microsoft.com>
2012-12-03 18:02:53 -0800

    Fixing unit test failures

--- test/Microsoft.AspNet.Mvc.Facebook.Test/Helpers/ContextHelpers.cs
+++ test/Microsoft.AspNet.Mvc.Facebook.Test/Helpers/ContextHelpers.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Web.Mvc;
 using Moq;
@@ -11,6 +12,7 @@ namespace Microsoft.AspNet.Mvc.Facebook.Test.Helpers
         public static ControllerContext CreateControllerContext(NameValueCollection requestFormData = null, NameValueCollection requestQueryData = null)
         {
             Mock<ControllerContext> controllerContext = new Mock<ControllerContext>();
+            controllerContext.Setup(c => c.HttpContext.Items).Returns(new Dictionary<object, object>());
             controllerContext.Setup(c => c.HttpContext.Request.Form).Returns(requestFormData ?? new NameValueCollection());
             controllerContext.Setup(c => c.HttpContext.Request.QueryString).Returns(requestQueryData ?? new NameValueCollection());
             return controllerContext.Object;

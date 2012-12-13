0fba05beed77347adeeba6b26361a370226443ee
youssefm <youssefm@microsoft.com>
2012-12-13 15:40:53 -0800

    [OData] Add XML documentation to the InlineCountValue enum values

--- src/System.Web.Http.OData/OData/Query/InlineCountValue.cs
+++ src/System.Web.Http.OData/OData/Query/InlineCountValue.cs
@@ -7,7 +7,13 @@ namespace System.Web.Http.OData.Query
     /// </summary>
     public enum InlineCountValue
     {
+        /// <summary>
+        /// Corresponds to the 'none' $inlinecount query option value.
+        /// </summary>
         None = 0,
+        /// <summary>
+        /// Corresponds to the 'allpages' $inlinecount query option value.
+        /// </summary>
         AllPages = 1
     }
 }

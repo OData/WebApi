a9ab5f01d932d59e4b2a258db27edfa500024929
youssefm <youssefm@microsoft.com>
2012-12-14 17:32:30 -0800

    [OData] Fast-path link generation for OData routes to improve formatter performance

--- src/System.Web.Http.OData/UrlHelperExtensions.cs
+++ src/System.Web.Http.OData/UrlHelperExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Web.Http.OData;
 using System.Web.Http.OData.Routing;
 using System.Web.Http.Routing;
@@ -30,13 +31,62 @@ namespace System.Web.Http
         /// <param name="urlHelper">The URL helper.</param>
         /// <param name="pathHandler">The path handler to use for generating the link.</param>
         /// <param name="segments">The OData path segments.</param>
-        /// <returns>The geerated OData link.</returns>
+        /// <returns>The generated OData link.</returns>
         public static string ODataLink(this UrlHelper urlHelper, IODataPathHandler pathHandler, IList<ODataPathSegment> segments)
         {
             string odataPath = pathHandler.Link(new ODataPath(segments));
+
+            string directLink = urlHelper.GenerateLinkDirectly(odataPath);
+            if (directLink != null)
+            {
+                return directLink;
+            }
+
+            // Slow path : use urlHelper.Link because the fast path failed
             return urlHelper.Link(
                 ODataRouteConstants.RouteName,
                 new HttpRouteValueDictionary() { { ODataRouteConstants.ODataPath, odataPath } });
+        }
+
+        // Fast path link generation where we recognize an OData route of the form "prefix/{*odataPath}".
+        // Link generation using HttpRoute.GetVirtualPath can consume up to 30% of processor time
+        // This is incredibly brittle code and should be removed whenever OData is upgraded to the next version of the runtime
+        // The right long-term fix is to remove this code and introduce an ODataRoute class that computes GetVirtualPath much faster
+        // But the long-term fix cannot be implemented because of a critical bug in WebAPI routing that affects custom routes in WebHost
+        // Removing this fast path is tracked by Issue 713
+        internal static string GenerateLinkDirectly(this UrlHelper urlHelper, string odataPath)
+        {
+            HttpRequestMessage request = urlHelper.Request;
+            HttpConfiguration config = request.GetConfiguration();
+            if (config != null)
+            {
+                IHttpRoute odataRoute;
+                if (config.Routes.TryGetValue(ODataRouteConstants.RouteName, out odataRoute))
+                {
+                    string routeTemplate = odataRoute.RouteTemplate;
+                    if (routeTemplate.EndsWith(ODataRouteConstants.ODataPathTemplate, StringComparison.Ordinal))
+                    {
+                        int odataPathTemplateIndex = routeTemplate.Length - ODataRouteConstants.ODataPathTemplate.Length;
+                        int indexOfFirstOpenBracket = routeTemplate.IndexOf('{');
+
+                        // We can only fast-path if there are no open brackets in the route prefix that need to be replaced
+                        // If there are, fall back to the slow path.
+                        if (indexOfFirstOpenBracket == odataPathTemplateIndex)
+                        {
+                            string virtualPathRoot = config.VirtualPathRoot;
+                            if (!virtualPathRoot.EndsWith("/", StringComparison.Ordinal))
+                            {
+                                virtualPathRoot += "/";
+                            }
+
+                            string routePrefix = routeTemplate.Substring(0, odataPathTemplateIndex);
+                            string link = virtualPathRoot + routePrefix + odataPath;
+                            return new Uri(request.RequestUri, link).AbsoluteUri;
+                        }
+                    }
+                }
+            }
+            return null;
         }
     }
 }
---
+++ test/System.Web.Http.OData.Test/UrlHelperExtensionsTest.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Net.Http;
+using System.Web.Http.Routing;
+using Microsoft.TestCommon;
+
+namespace System.Web.Http
+{
+    public class UrlHelperExtensionsTest
+    {
+        [Fact]
+        public void GenerateLinkDirectly_ReturnsNull_IfHelperRequestHasNoConfiguration()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/vpath/prefix/Customers");
+            UrlHelper urlHelper = new UrlHelper(request);
+
+            Assert.Null(urlHelper.GenerateLinkDirectly("odataPath"));
+        }
+
+        [Fact]
+        public void GenerateLinkDirectly_ReturnsNull_IfNoRouteCalledODataFound()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/vpath/prefix/Customers");
+            HttpConfiguration config = new HttpConfiguration(new HttpRouteCollection("http://localhost/vpath"));
+            config.Routes.MapHttpRoute("NotOData", "{controller}");
+            request.Properties["MS_HttpConfiguration"] = config;
+            UrlHelper urlHelper = new UrlHelper(request);
+
+            Assert.Null(urlHelper.GenerateLinkDirectly("odataPath"));
+        }
+
+        [Fact]
+        public void GenerateLinkDirectly_ReturnsNull_IfRouteTemplateDoesNotEndInODataPath()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/vpath/prefix/Customers");
+            HttpConfiguration config = new HttpConfiguration(new HttpRouteCollection("http://localhost/vpath"));
+            config.Routes.MapHttpRoute("OData", "prefix/{*notODataPath}");
+            request.Properties["MS_HttpConfiguration"] = config;
+            UrlHelper urlHelper = new UrlHelper(request);
+
+            Assert.Null(urlHelper.GenerateLinkDirectly("odataPath"));
+        }
+
+        [Fact]
+        public void GenerateLinkDirectly_ReturnsNull_IfRouteTemplateHasParameterInPrefix()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/vpath/prefix/Customers");
+            HttpConfiguration config = new HttpConfiguration(new HttpRouteCollection("http://localhost/vpath"));
+            config.Routes.MapHttpRoute("OData", "{prefix}/{*odataPath}");
+            request.Properties["MS_HttpConfiguration"] = config;
+            UrlHelper urlHelper = new UrlHelper(request);
+
+            Assert.Null(urlHelper.GenerateLinkDirectly("odataPath"));
+        }
+
+        [Fact]
+        public void GenerateLinkDirectly_ReturnsUri_IfConditionsSatisfied()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/vpath/prefix/Customers");
+            HttpConfiguration config = new HttpConfiguration(new HttpRouteCollection("http://localhost/vpath"));
+            config.Routes.MapHttpRoute("OData", "prefix/{*odataPath}");
+            request.Properties["MS_HttpConfiguration"] = config;
+            request.Properties["MS_HttpRouteData"] = new HttpRouteData(new HttpRoute());
+            UrlHelper urlHelper = new UrlHelper(request);
+
+            Assert.Equal("http://localhost/vpath/prefix/odataPath", urlHelper.GenerateLinkDirectly("odataPath"));
+        }
+    }
+}

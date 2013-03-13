56a382015524d6e6a117a6691c2ad668988153ca
Tim McCall <mccalltd@gmail.com>
2013-03-13 16:01:48 -0600

    add route template and name support to HttpDeleteAttribute.

--- src/System.Web.Http/HttpDeleteAttribute.cs
+++ src/System.Web.Http/HttpDeleteAttribute.cs
@@ -2,14 +2,23 @@
 
 using System.Collections.ObjectModel;
 using System.Net.Http;
-using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
 
 namespace System.Web.Http
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    public sealed class HttpDeleteAttribute : Attribute, IActionHttpMethodProvider
+    public sealed class HttpDeleteAttribute : Attribute, IHttpRouteProvider
     {
         private static readonly Collection<HttpMethod> _supportedMethods = new Collection<HttpMethod>(new HttpMethod[] { HttpMethod.Delete });
+
+        public HttpDeleteAttribute()
+        {
+        }
+
+        public HttpDeleteAttribute(string routeTemplate)
+        {
+            RouteTemplate = routeTemplate;
+        }
 
         public Collection<HttpMethod> HttpMethods
         {
@@ -18,5 +27,9 @@ namespace System.Web.Http
                 return _supportedMethods;
             }
         }
+
+        public string RouteName { get; set; }
+        
+        public string RouteTemplate { get; private set; }
     }
 }

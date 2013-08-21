d538298b2d197e6a9f6f557abd9630bdd2a0eecd
davidmatson <dmatson@microsoft.com>
2013-08-21 15:27:30 -0700

    Incorporate code review feedback.

--- src/System.Web.Http/Controllers/CandidateAction.cs
+++ src/System.Web.Http/Controllers/CandidateAction.cs
@@ -1,12 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Web.Http.Controllers;
-using System.Web.Http.Properties;
 
 namespace System.Web.Http.Routing
 {

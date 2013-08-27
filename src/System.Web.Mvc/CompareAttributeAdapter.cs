44b27bed96a83afd0acae7e4e8cf3c203433b219
yaohuang <yaohuang@microsoft.com>
2013-08-27 10:50:00 -0700

    654045 Data validation System.ComponentModel.DataAnnotations.Compare does not inject the Display value for a custom ErrorMessage The problem here is that the CompareAttribute from System.ComponentModel.DataAnnotations doesn't populate the OtherPropertyDisplayName property until IsValid() is called. Therefore, by the time CompareAttributeAdapter tries to generate the error message for client validation, the OtherPropertyDisplayName hasn't been populated and the error message falls back on using the OtherProperty name. The proposed fix is to eagerly get the value for OtherPropertyDisplayName similar to what we do in System.Web.Mvc.CompareAttribute. Because the OtherPropertyDisplayName property is not settable, I had to create an wrapper and override the FormatErrorMessage method.

--- src/System.Web.Mvc/CompareAttributeAdapter.cs
+++ src/System.Web.Mvc/CompareAttributeAdapter.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Globalization;
 using DataAnnotationsCompareAttribute = System.ComponentModel.DataAnnotations.CompareAttribute;
 
 namespace System.Web.Mvc
@@ -9,7 +10,7 @@ namespace System.Web.Mvc
     internal class CompareAttributeAdapter : DataAnnotationsModelValidator<DataAnnotationsCompareAttribute>
     {
         public CompareAttributeAdapter(ModelMetadata metadata, ControllerContext context, DataAnnotationsCompareAttribute attribute)
-            : base(metadata, context, attribute)
+            : base(metadata, context, new CompareAttributeWrapper(attribute, metadata))
         {
             Contract.Assert(attribute.GetType() == typeof(DataAnnotationsCompareAttribute));
         }
@@ -24,6 +25,29 @@ namespace System.Web.Mvc
             Contract.Assert(property != null);
 
             return "*." + property;
+        }
+
+        // Wrapper for CompareAttribute that will eagerly get the OtherPropertyDisplayName and use it on the error message for client validation.
+        // The System.ComponentModel.DataAnnotations.CompareAttribute doesn't populate the OtherPropertyDisplayName until after IsValid() 
+        // is called. Therefore, by the time we get the error message for client validation, the display name is not populated and won't be used.
+        private sealed class CompareAttributeWrapper : DataAnnotationsCompareAttribute
+        {
+            private readonly string _otherPropertyDisplayName;
+
+            public CompareAttributeWrapper(DataAnnotationsCompareAttribute attribute, ModelMetadata metadata)
+                : base(attribute.OtherProperty)
+            {
+                _otherPropertyDisplayName = attribute.OtherPropertyDisplayName;
+                if (_otherPropertyDisplayName == null && metadata.ContainerType != null)
+                {
+                    _otherPropertyDisplayName = ModelMetadataProviders.Current.GetMetadataForProperty(() => metadata.Model, metadata.ContainerType, attribute.OtherProperty).GetDisplayName();
+                }
+            }
+
+            public override string FormatErrorMessage(string name)
+            {
+                return String.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, _otherPropertyDisplayName ?? OtherProperty);
+            }
         }
     }
 }
---
+++ test/System.Web.Mvc.Test/Test/CompareAttributeAdapterTest.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.TestCommon;
+
+namespace System.Web.Mvc.Test
+{
+    public class CompareAttributeAdapterTest
+    {
+        [Fact]
+        public void ClientRulesWithCompareAttribute_ErrorMessageUsesDisplayName()
+        {
+            // Arrange
+            var metadata = ModelMetadataProviders.Current.GetMetadataForProperty(() => null, typeof(PropertyDisplayNameModel), "MyProperty");
+            var context = new ControllerContext();
+            var attribute = new System.ComponentModel.DataAnnotations.CompareAttribute("OtherProperty");
+            var adapter = new CompareAttributeAdapter(metadata, context, attribute);
+
+            // Act
+            var rules = adapter.GetClientValidationRules()
+                .OrderBy(r => r.ValidationType)
+                .ToArray();
+
+            // Assert
+            ModelClientValidationRule rule = Assert.Single(rules);
+            Assert.Equal("'MyPropertyDisplayName' and 'OtherPropertyDisplayName' do not match.", rule.ErrorMessage);
+        }
+
+        [Fact]
+        public void ClientRulesWithCompareAttribute_ErrorMessageUsesPropertyName()
+        {
+            // Arrange
+            var metadata = ModelMetadataProviders.Current.GetMetadataForProperty(() => null, typeof(PropertyNameModel), "MyProperty");
+            var context = new ControllerContext();
+            var attribute = new System.ComponentModel.DataAnnotations.CompareAttribute("OtherProperty");
+            var adapter = new CompareAttributeAdapter(metadata, context, attribute);
+
+            // Act
+            var rules = adapter.GetClientValidationRules()
+                .OrderBy(r => r.ValidationType)
+                .ToArray();
+
+            // Assert
+            ModelClientValidationRule rule = Assert.Single(rules);
+            Assert.Equal("'MyProperty' and 'OtherProperty' do not match.", rule.ErrorMessage);
+        }
+
+        private class PropertyDisplayNameModel
+        {
+            [DisplayName("MyPropertyDisplayName")]
+            public string MyProperty { get; set; }
+
+            [DisplayName("OtherPropertyDisplayName")]
+            public string OtherProperty { get; set; }
+        }
+
+        private class PropertyNameModel
+        {
+            public string MyProperty { get; set; }
+
+            public string OtherProperty { get; set; }
+        }
+    }
+}

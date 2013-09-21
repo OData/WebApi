10f8cbc1f03200913e82a800ad863f2c495a3997
Yishai Galatzer <yigalatz@microsoft.com>
2013-09-20 17:16:11 -0700

    Downgrade razor version back to 3.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Microsoft ASP.NET MVC")]
 #elif ASPNETWEBPAGES
 #if !BUILD_GENERATED_VERSION
-[assembly: AssemblyVersion("3.1.0.0")] // ASPNETWEBPAGES
-[assembly: AssemblyFileVersion("3.1.0.0")] // ASPNETWEBPAGES
+[assembly: AssemblyVersion("3.0.0.0")] // ASPNETWEBPAGES
+[assembly: AssemblyFileVersion("3.0.0.0")] // ASPNETWEBPAGES
 #endif
 [assembly: AssemblyProduct("Microsoft ASP.NET Web Pages")]
 #else

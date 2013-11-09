1bfee49f633ecec15cdc0615adeb6f269f5cdd46
dougbu <dougbu@microsoft.com>
2013-11-08 16:05:08 -0800

    Get build working in any command window - Prefer VS 2013 (v12.0) version of MSBuild.exe and fallback to v4.0 when necessary - Do not rely on %Path% since that limits us to VS developer cmd windows - Match Microsoft.Build.Tasks*.dll version to MSBuild.exe, not type of cmd window - Have tested building in VS 2013, VS 2013 dev cmd window, and bare cmd window
    Requires either a FxCopCmd.exe.config binding redirect or David's "VS2013 FxCop
    compatibility" change for full VS 2013 support.

--- build.cmd
+++ build.cmd
@@ -1,18 +1,23 @@
 @echo off
 pushd %~dp0
+setlocal
 
 if exist bin goto build
 mkdir bin
 
 :Build
+set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MsBuild.exe"
+if not exist %MSBuild% @set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
 if "%1" == "" goto BuildDefaults
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /t:%* /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%MSBuild% Runtime.msbuild /m /nr:false /t:%* /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
 :BuildDefaults
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild Runtime.msbuild /m /nr:false /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
+%MSBuild% Runtime.msbuild /m /nr:false /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
@@ -28,3 +33,4 @@ goto end
 
 :End
 popd
+endlocal

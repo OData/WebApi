1e55e5173c467c9106b697620b1939676ed2806c
dougbu <dougbu@microsoft.com>
2013-11-10 11:58:10 -0800

    Add comment for %MSBuild% setting - CR follow-up

--- build.cmd
+++ build.cmd
@@ -6,8 +6,12 @@ if exist bin goto build
 mkdir bin
 
 :Build
-set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe"
-if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MsBuild.exe"
+
+REM Find the most recent 32bit MSBuild.exe on the system. If v12.0 (installed with VS2013) does not exist, fall back to
+REM v4.0. Also handle x86 operating systems, where %ProgramFiles(x86)% is not defined. Always quote the %MSBuild% value
+REM when setting the variable and never quote %MSBuild% references.
+set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %MSBuild% @set MSBuild="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 
 if "%1" == "" goto BuildDefaults

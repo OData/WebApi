f6a83be0726cde470b6f1931feba4aad9a74b233
raghuramn <ranadimi@microsoft.com>
2013-11-26 19:03:44 -0800

    Issue 1340: support modelbinding nonstring data from route data
    Initial fix for this issue is done with commit a14733ff074 which has a
    breaking change. Commit 6b64e226a has the non-breaking change but it
    didn't revert the breaking change done in commit a14733ff074. This change
    just reverts the inital fix (a14733ff074).

--- src/System.Web.Http/ValueProviders/Providers/RouteDataValueProvider.cs
+++ src/System.Web.Http/ValueProviders/Providers/RouteDataValueProvider.cs
@@ -1,15 +1,26 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.Globalization;
 using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
 
 namespace System.Web.Http.ValueProviders.Providers
 {
     public class RouteDataValueProvider : NameValuePairsValueProvider
     {
         public RouteDataValueProvider(HttpActionContext actionContext, CultureInfo culture)
-            : base(actionContext.ControllerContext.RouteData.Values, culture)
+            : base(GetRouteValues(actionContext.ControllerContext.RouteData), culture)
         {
+        }
+
+        internal static IEnumerable<KeyValuePair<string, string>> GetRouteValues(IHttpRouteData routeData)
+        {
+            foreach (KeyValuePair<string, object> pair in routeData.Values)
+            {
+                string value = (pair.Value == null) ? null : pair.Value.ToString();
+                yield return new KeyValuePair<string, string>(pair.Key, value);
+            }
         }
     }
 }
--- test/System.Web.Http.Test/ValueProviders/Providers/RouteDataValueProviderTest.cs
+++ test/System.Web.Http.Test/ValueProviders/Providers/RouteDataValueProviderTest.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-using System.Globalization;
-using System.Net.Http;
-using System.Web.Http.Controllers;
+using System.Collections.Generic;
 using System.Web.Http.Routing;
 using Microsoft.TestCommon;
 
@@ -11,50 +9,32 @@ namespace System.Web.Http.ValueProviders.Providers
     public class RouteDataValueProviderTest
     {
         [Fact]
-        public void GetValue_RouteDataContainsNonStringValue()
+        public void GetRouteValues_Returns_RouteValues()
         {
             // Arrange
-            var data = new TestClass();
             HttpRouteData routeData = new HttpRouteData(new HttpRoute());
-            routeData.Values["item"] = data;
-            HttpActionContext context = new HttpActionContext();
-            context.ControllerContext = new HttpControllerContext { RouteData = routeData };
-            RouteDataValueProvider provider = new RouteDataValueProvider(context, CultureInfo.InvariantCulture);
+            routeData.Values.Add("key1", "value1");
+            routeData.Values.Add("key2", "value2");
 
             // Act
-            ValueProviderResult result = provider.GetValue("item");
+            IEnumerable<KeyValuePair<string, string>> result = RouteDataValueProvider.GetRouteValues(routeData);
 
-            //Assert
-            Assert.Same(data, result.RawValue);
+            // Assert
+            Assert.Equal(result, new Dictionary<string, string> { { "key1", "value1" }, { "key2", "value2" } });
         }
 
         [Fact]
-        public void CanModelBindNonStringData()
+        public void GetRouteValues_IfRouteDataHasNullValue_ReturnsKeyValuePairWithNullValue()
         {
             // Arrange
-            HttpServer server = new HttpServer();
-            server.Configuration.Routes.MapHttpRoute("default", "{controller}", new { test = new TestClass { Id = 42 } });
-            HttpClient client = new HttpClient(server);
+            HttpRouteData routeData = new HttpRouteData(new HttpRoute());
+            routeData.Values.Add("key", null);
 
             // Act
-            var response = client.GetAsync("http://localhost/Test").Result;
+            IEnumerable<KeyValuePair<string, string>> result = RouteDataValueProvider.GetRouteValues(routeData);
 
             // Assert
-            var testClass = response.Content.ReadAsAsync<TestClass>().Result;
-            Assert.Equal(42, testClass.Id);
-        }
-
-        public class TestClass
-        {
-            public int Id { get; set; }
-        }
-
-        public class TestController : ApiController
-        {
-            public TestClass Get([FromUri]TestClass test)
-            {
-                return test;
-            }
+            Assert.Equal(result, new[] { new KeyValuePair<string, string>("key", null) });
         }
     }
 }

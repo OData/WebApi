6137c59a4f50790c21d5f6d36aea606e3a63b48d
Ryan Nowak <nowakra@gmail.com>
2013-12-13 14:08:01 -0800

    Codeplex-1431 Support buffered async request stream in web host
    This change allows WebAPI to use the async buffered request stream
    in web host.
    
    There are two ways to get a buffered request stream in webhost:
    - GetBufferedInputStream(): is async, and buffered, but not seekable
    - InputStream: is buffered and seekable, but not async
    
    This causes a problem, because async is good... but we need seekable
    streams, as we've surfaced this functionality to the user-code.
    
    Conveniently, once you've read to the end of the GetBufferedInputStream,
    it will also fully populate the buffer of InputStream. The fact that the
    buffer of InputStream is fully populated means that its non-async
    operations become preferrable once the buffer is finished loading, so we
    have a stream that's seekable, and buffered, and we no longer care about
    async at that point.
    
    The solution is to wrap up both of these streams, so that once we've read
    to the end of the 'async' stream, we start using the 'non-async' stream.
    This offers the performance advantages of both cases, while preserving
    seekability.
    
    The special case we have to handle for seek is when the 'non-seekable' stream
    is being used, we finish reading the end and then swap to the 'seekable'
    stream. This needs to be done syncronously because of the contract for
    Seek. This will only really occur in cases where user code gets the stream
    and manually does seek operations instead of just reading it. Basically we
    don't expect this case to be common, and it should perform about as well as it
    currently does.
    
    Katana already has a similar solution in place, and I borrowed some ideas
    from them - our version is a bit simpler because we have simpler
    requirements.
    
    (cherry picked from commit addff083c4cf60c390ccb1e217fde4a369004b99)
    (cherry picked from commit 0bf8e5c7016e524b937a72e2e812d9ea0b191527)

---
+++ test/System.Web.Http.WebHost.Test/SeekableBufferedRequestStreamTest.cs
@@ -0,0 +1,277 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Web.Http.WebHost
+{
+    public class SeekableBufferedRequestStreamTest
+    {
+        private const string Content = "Hello, World!";
+
+        /// <summary>
+        /// Chosen to require multiple reads.
+        /// </summary>
+        private const int BufferSize = 3;
+
+        [Fact]
+        public void ReadToEnd_WithRead_SwapsStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Act
+            byte[] buffer = new byte[BufferSize];
+            while (stream.Read(buffer, 0, buffer.Length) > 0)
+            {
+                // Guard
+                Assert.Same(nonSeekable, stream.InnerStream);
+            }
+
+            // Assert
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void ReadToEnd_WithBeginRead_SwapsStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Act
+            byte[] buffer = new byte[BufferSize];
+            while (stream.EndRead(stream.BeginRead(buffer, 0, buffer.Length, null, null)) > 0)
+            {
+                // Guard
+                Assert.Same(nonSeekable, stream.InnerStream);
+            }
+
+            // Assert
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public async Task ReadToEnd_WithReadAsync_SwapsStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Act
+            byte[] buffer = new byte[BufferSize];
+            while (await stream.ReadAsync(buffer, 0, buffer.Length) > 0)
+            {
+                // Guard
+                Assert.Same(nonSeekable, stream.InnerStream);
+            }
+
+            // Assert
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void ReadToEnd_WithReadByte_SwapsStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Act
+            while (stream.ReadByte() > 0)
+            {
+                // Guard
+                Assert.Same(nonSeekable, stream.InnerStream);
+            }
+
+            // Assert
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void SwapStream_PreservesPosition()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            stream.ReadByte();
+
+            // Guard
+            Assert.Same(nonSeekable, stream.InnerStream);
+            Assert.Equal(1L, stream.Position);
+
+            stream.Seek(2L, SeekOrigin.Begin);
+
+            // Assert
+            Assert.Equal(2L, stream.Position);
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void Seek_SwapsStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Guard
+            Assert.Same(nonSeekable, stream.InnerStream);
+
+            // Act
+            stream.Seek(1L, SeekOrigin.Begin);
+
+            // Assert
+            Assert.Same(seekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void Seek_NoOpBegin()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            for (int i = 0; i < 3; i++)
+            {
+                stream.ReadByte();
+            }
+
+            // Act
+            stream.Seek(3L, SeekOrigin.Begin);
+
+            // Assert
+            Assert.Same(nonSeekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void Seek_NoOpCurent()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            for (int i = 0; i < 3; i++)
+            {
+                stream.ReadByte();
+            }
+
+            // Act
+            stream.Seek(0L, SeekOrigin.Current);
+
+            // Assert
+            Assert.Same(nonSeekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void Seek_NoOpEnd()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            for (int i = 0; i < 3; i++)
+            {
+                stream.ReadByte();
+            }
+
+            // Act
+            stream.Seek(stream.Position - stream.Length, SeekOrigin.End);
+
+            // Assert
+            Assert.Same(nonSeekable, stream.InnerStream);
+        }
+
+        [Fact]
+        public void Dispose_DoesNotDisposeInnerStreams()
+        {
+            // Arrange
+            var nonSeekable = CreateNonSeekableStream(Content);
+            var seekable = CreateSeekableStream(Content);
+            var stream = CreateStream(nonSeekable, seekable);
+
+            // Act
+            stream.Dispose();
+
+            // Assert
+            Assert.False(stream.CanRead);
+            Assert.True(nonSeekable.CanRead);
+            Assert.True(seekable.CanRead);
+        }
+
+        private Stream CreateSeekableStream(string content)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(content));
+        }
+
+        private Stream CreateNonSeekableStream(string content)
+        {
+            return new NonSeekableStream(Encoding.UTF8.GetBytes(content));
+        }
+
+        private AccessibleStreamWrapper CreateStream(Stream stream1, Stream stream2)
+        {
+            // Guards
+            Assert.False(stream1.CanSeek);
+            Assert.True(stream2.CanSeek);
+
+            var request = new Mock<HttpRequestBase>();
+            request.Setup(r => r.GetBufferedInputStream()).Returns(stream1);
+            request.SetupGet(r => r.InputStream).Returns(stream2);
+
+            return new AccessibleStreamWrapper(request.Object);
+        }
+
+        private class AccessibleStreamWrapper : SeekableBufferedRequestStream
+        {
+            public AccessibleStreamWrapper(HttpRequestBase request)
+                : base(request)
+            {
+            }
+
+            public new Stream InnerStream
+            {
+                get
+                {
+                    return base.InnerStream;
+                }
+            }
+        }
+
+        private class NonSeekableStream : MemoryStream
+        {
+            public NonSeekableStream()
+            {
+            }
+
+            public NonSeekableStream(byte[] bytes)
+                : base(bytes)
+            {
+            }
+
+            public override bool CanSeek
+            {
+                get
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}

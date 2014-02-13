a12032f07d71f0f8a4f846604e8bac528d8d83f5
jacalvar <jacalvar@microsoft.com>
2014-02-13 15:19:21 -0800

    Issue 1423 - Support ComplexTypeAttribute, AssociationAttribute, TimestampAttribute and ConcurrencyCheckAttribute in ODataConventionModelBuilder https://aspnetwebstack.codeplex.com/workitem/1423
    Added support for ComplexTypeAttribute and TimestampAttribute, support for
    ConcurrencyCheckAttribute already existed. Support for AssociationLinkAttribute
    requires changes on the way we build the model that are out of the scope of
    this fix.

---
+++ test/System.Web.OData.Test/OData/Builder/Conventions/Attributes/ComplexTypeAttributeConventionTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Linq;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Web.OData.Builder.Conventions.Attributes
+{
+    public class ComplexTypeAttributeConventionTests
+    {
+        [Fact]
+        public void ComplexTypeConvention_RemovesAnyKeyFromEntityConfiguration()
+        {
+            // Arrange
+            EntityTypeConfiguration entity = CreateEntitytypeConfigurationMock();
+            entity.HasKey(new MockPropertyInfo(typeof(int), "Id"));
+            ComplexTypeAttributeConvention convention = new ComplexTypeAttributeConvention();
+
+            // Act
+            convention.Apply(entity, null, null);
+
+            // Assert
+            Assert.Equal(0, entity.Keys.Count());
+        }
+
+        [Fact]
+        public void ComplexTypeConvention_DoesntApplyToExplicitlyDefinedTypes()
+        {
+            // Arrange
+            EntityTypeConfiguration entity = CreateEntitytypeConfigurationMock();
+            entity.AddedExplicitly = true;
+            entity.HasKey(new MockPropertyInfo(typeof(int), "Id"));
+            ComplexTypeAttributeConvention convention = new ComplexTypeAttributeConvention();
+
+            // Act
+            convention.Apply(entity, null, null);
+
+            // Assert
+            Assert.Equal(1, entity.Keys.Count());
+        }
+
+        private static EntityTypeConfiguration CreateEntitytypeConfigurationMock()
+        {
+            Mock<EntityTypeConfiguration> entity = new Mock<EntityTypeConfiguration>();
+            entity.Setup(e => e.ModelBuilder).Returns(new ODataModelBuilder());
+            entity.Setup(e => e.ClrType).Returns(typeof(object));
+            entity.CallBase = true;
+            return entity.Object;
+        }
+    }
+}
---
+++ test/System.Web.OData.Test/OData/Builder/Conventions/Attributes/TimestampAttributeEdmPropertyConventionTests.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Web.OData.Builder.Conventions.Attributes
+{
+    public class TimestampAttributeEdmPropertyConventionTests
+    {
+        [Fact]
+        public void TimestampConvention_AppliesWhenTheAttributeIsAppliedToASingleProperty()
+        {
+            // Arrange
+            PropertyInfo property = CreateMockPropertyInfo("TestProperty");
+            EntityTypeConfiguration entityType = new EntityTypeConfiguration();
+            PrimitivePropertyConfiguration primitiveProperty = new PrimitivePropertyConfiguration(property, entityType);
+            entityType.ExplicitProperties.Add(property, primitiveProperty);
+            TimestampAttributeEdmPropertyConvention convention = new TimestampAttributeEdmPropertyConvention();
+
+            // Act
+            convention.Apply(primitiveProperty, entityType);
+
+            // Assert
+            Assert.True(primitiveProperty.ConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampConvention_DoesntApplyWhenTheAttributeIsAppliedOnANonEntityType()
+        {
+            // Arrange
+            PropertyInfo property = CreateMockPropertyInfo("TestProperty");
+            ComplexTypeConfiguration complexType = new ComplexTypeConfiguration();
+            PrimitivePropertyConfiguration primitiveProperty = new PrimitivePropertyConfiguration(property, complexType);
+            complexType.ExplicitProperties.Add(property, primitiveProperty);
+            TimestampAttributeEdmPropertyConvention convention = new TimestampAttributeEdmPropertyConvention();
+
+            // Act
+            convention.Apply(primitiveProperty, complexType);
+
+            // Assert
+            Assert.False(primitiveProperty.ConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampConvention_DoesntApplyWhenTheAttributeIsAppliedToMultipleProperties()
+        {
+            // Arrange
+            PropertyInfo property = CreateMockPropertyInfo("TestProperty");
+            PropertyInfo otherProperty = CreateMockPropertyInfo("OtherTestProperty");
+            EntityTypeConfiguration entityType = new EntityTypeConfiguration();
+            PrimitivePropertyConfiguration primitiveProperty = new PrimitivePropertyConfiguration(property, entityType);
+            entityType.ExplicitProperties.Add(property, primitiveProperty);
+            entityType.ExplicitProperties.Add(otherProperty, new PrimitivePropertyConfiguration(otherProperty, entityType));
+            TimestampAttributeEdmPropertyConvention convention = new TimestampAttributeEdmPropertyConvention();
+
+            // Act
+            convention.Apply(primitiveProperty, entityType);
+
+            // Assert
+            Assert.False(primitiveProperty.ConcurrencyToken);
+        }
+
+        [Fact]
+        public void TimestampConvention_DoesntApplyWhenTheAttributeIsAppliedToMultipleProperties_InATypeHierarchy()
+        {
+            // Arrange
+            PropertyInfo property = CreateMockPropertyInfo("TestProperty");
+            PropertyInfo otherProperty = CreateMockPropertyInfo("OtherTestProperty");
+            EntityTypeConfiguration baseEntityType = new EntityTypeConfiguration();
+            EntityTypeConfiguration entityType = new Mock<EntityTypeConfiguration>().SetupAllProperties().Object;
+            entityType.BaseType = baseEntityType;
+            PrimitivePropertyConfiguration primitiveProperty = new PrimitivePropertyConfiguration(property, entityType);
+            entityType.ExplicitProperties.Add(property, primitiveProperty);
+            baseEntityType.ExplicitProperties.Add(otherProperty, new PrimitivePropertyConfiguration(otherProperty, baseEntityType));
+            TimestampAttributeEdmPropertyConvention convention = new TimestampAttributeEdmPropertyConvention();
+
+            // Act
+            convention.Apply(primitiveProperty, entityType);
+
+            // Assert
+            Assert.False(primitiveProperty.ConcurrencyToken);
+        }
+
+        private static PropertyInfo CreateMockPropertyInfo(string propertyName)
+        {
+            Mock<PropertyInfo> property = new Mock<PropertyInfo>();
+            property.Setup(p => p.Name).Returns(propertyName);
+            property.Setup(p => p.PropertyType).Returns(typeof(int));
+            property.Setup(p => p.GetCustomAttributes(It.IsAny<bool>())).Returns(new object[] { new TimestampAttribute() });
+            property.Setup(p => p.GetCustomAttributes(typeof(TimestampAttribute), It.IsAny<bool>())).Returns(new object[] { new TimestampAttribute() });
+            return property.Object;
+        }
+    }
+}

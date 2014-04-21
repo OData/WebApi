8dd74d2a69c31a3279df24c2010e875ea5b49514
Pranav K <prkrishn@hotmail.com>
2014-04-21 15:03:53 -0700

    Adding support for overriding unobtrusive validation defaults
    Work Item: 1502

--- src/System.Web.Mvc/JavaScript/jquery.validate.unobtrusive.js
+++ src/System.Web.Mvc/JavaScript/jquery.validate.unobtrusive.js
@@ -101,18 +101,32 @@
     function validationInfo(form) {
         var $form = $(form),
             result = $form.data(data_validation),
-            onResetProxy = $.proxy(onReset, form);
+            onResetProxy = $.proxy(onReset, form),
+            defaultOptions = $jQval.unobtrusive.options || {},
+            execInContext = function (name, args) {
+                var func = defaultOptions[name];
+                func && $.isFunction(func) && func.apply(form, args);
+            }
 
         if (!result) {
             result = {
                 options: {  // options structure passed to jQuery Validate's validate() method
-                    errorClass: "input-validation-error",
-                    errorElement: "span",
-                    errorPlacement: $.proxy(onError, form),
-                    invalidHandler: $.proxy(onErrors, form),
+                    errorClass: defaultOptions.errorClass || "input-validation-error",
+                    errorElement: defaultOptions.errorElement || "span",
+                    errorPlacement: function () {
+                        onError.apply(form, arguments);
+                        execInContext("errorPlacement", arguments);
+                    },
+                    invalidHandler: function () {
+                        onErrors.apply(form, arguments);
+                        execInContext("invalidHandler", arguments);
+                    },
                     messages: {},
                     rules: {},
-                    success: $.proxy(onSuccess, form)
+                    success: function () {
+                        onSuccess.apply(form, arguments);
+                        execInContext("success", arguments);
+                    }
                 },
                 attachValidation: function () {
                     $form

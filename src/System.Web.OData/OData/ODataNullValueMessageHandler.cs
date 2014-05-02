6ae0ac74bf0048537fa16e1931807238f25d59f3
jacalvar <jacalvar@microsoft.com>
2014-05-01 21:16:59 -0700

    Issue 1718 - System.ArgumentNullException is thrown if a raw value of null is requested. https://aspnetwebstack.codeplex.com/workitem/1718
    This change fixes the broader problem by handling null values in general,
    not just for raw values. We replaced ODataNullValueAttribute with
    ODataNullValueMessageHandler. This message handler will convert null
    requests for individual entities or singletons into 404 responses and
    will convert null requests for non collection properties (navigation
    or structural) into 204 responses. This behaviour is not enabled by
    default. The user needs to plug in the message handler as a per route
    handler when he registers the OData route.

---
+++ src/System.Web.OData/OData/ODataNullValueMessageHandler.cs
@@ -0,0 +1,151 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing;
+using Microsoft.OData.Edm;
+
+namespace System.Web.OData
+{
+    /// <summary>
+    /// Represents an <see href="HttpMessageHandler" /> that converts null values in OData responses to
+    /// HTTP NotFound responses or NoContent responses following the OData specification.
+    /// </summary>
+    public class ODataNullValueMessageHandler : DelegatingHandler
+    {
+        /// <inheritdoc />
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw Error.ArgumentNull("request");
+            }
+
+            HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+            // This message handler is intended for helping with queries that return a null value, for example in a
+            // get request for a particular entity on an entity set, for a single valued navigation property or for
+            // a structural property of a given entity. The only case in which a data modification request will result
+            // in a 204 response status code, is when a primitive property is set to null through a PUT request to the
+            // property URL and in that case, the user can return the right status code himself.
+            ObjectContent content = response == null ? null : response.Content as ObjectContent;
+            if (request.Method == HttpMethod.Get && content != null && content.Value == null &&
+                response.StatusCode == HttpStatusCode.OK)
+            {
+                HttpStatusCode? newStatusCode = GetUpdatedResponseStatusCodeOrNull(request.ODataProperties().Path);
+                if (newStatusCode.HasValue)
+                {
+                    response = request.CreateResponse(newStatusCode.Value);
+                }
+            }
+
+            return response;
+        }
+
+        // This method is intended for unit testing purposes only.
+        internal Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            return SendAsync(request, CancellationToken.None);
+        }
+
+        // Determines if a status code needs to be changed based on the path of the request and returns the new
+        // status code or null if no change is required.
+        internal static HttpStatusCode? GetUpdatedResponseStatusCodeOrNull(ODataPath oDataPath)
+        {
+            if (oDataPath == null || oDataPath.Segments == null || oDataPath.Segments.Count == 0)
+            {
+                return null;
+            }
+
+            // Skip any sequence of cast segments at the end of the path.
+            int currentIndex = oDataPath.Segments.Count - 1;
+            ReadOnlyCollection<ODataPathSegment> segments = oDataPath.Segments;
+            while (currentIndex >= 0 && segments[currentIndex].SegmentKind == ODataSegmentKinds.Cast)
+            {
+                currentIndex--;
+            }
+
+            // Null value properties should be treated in the same way independent of whether the user asked for the
+            // raw value of the property or a specific format, so we skip the $value segment as it can only be
+            // preceeded by a property access segment.
+            if (currentIndex >= 0 && segments[currentIndex].SegmentKind == ODataSegmentKinds.Value)
+            {
+                currentIndex--;
+            }
+
+            // Protect ourselves against malformed path segments.
+            if (currentIndex < 0)
+            {
+                return null;
+            }
+
+            switch (segments[currentIndex].SegmentKind)
+            {
+                case ODataSegmentKinds._Key:
+                    // Look at the previous segment to decide, but skip any possible sequence of cast segments in 
+                    // between.
+                    currentIndex--;
+                    while (currentIndex >= 0 && segments[currentIndex].SegmentKind == ODataSegmentKinds.Cast)
+                    {
+                        currentIndex--;
+                    }
+                    if (currentIndex < 0)
+                    {
+                        return null;
+                    }
+
+                    switch (segments[currentIndex].SegmentKind)
+                    {
+                        case ODataSegmentKinds._EntitySet:
+                            // Return 404 if we were trying to retrieve a specific entity from an entity set.
+                            return HttpStatusCode.NotFound;
+                        case ODataSegmentKinds._Navigation:
+                            // Return 204 if we were trying to retrieve a related entity via a navigation property.
+                            return HttpStatusCode.NoContent;
+                        default:
+                            break;
+                    }
+                    return null;
+
+                case ODataSegmentKinds._Property:
+                    // Return 204 only if the property is single valued (not a collection of values).
+                    PropertyAccessPathSegment property = (PropertyAccessPathSegment)segments[currentIndex];
+                    return GetChangedStatusCodeForProperty(property);
+
+                case ODataSegmentKinds._Navigation:
+                    // Return 204 only if the navigation property is a single related entity and not a collection
+                    // of entities.
+                    NavigationPathSegment navigation = (NavigationPathSegment)segments[currentIndex];
+                    return GetChangedStatusCodeForNavigationProperty(navigation);
+
+                case ODataSegmentKinds._Singleton:
+                    // Return 404 for a singleton with a null value.
+                    return HttpStatusCode.NotFound;
+
+                default:
+                    return null;
+            }
+        }
+
+        private static HttpStatusCode? GetChangedStatusCodeForNavigationProperty(NavigationPathSegment navigation)
+        {
+            EdmMultiplicity multiplicity = navigation.NavigationProperty.TargetMultiplicity();
+            return multiplicity == EdmMultiplicity.ZeroOrOne || multiplicity == EdmMultiplicity.One ?
+                HttpStatusCode.NoContent :
+                (HttpStatusCode?)null;
+        }
+
+        private static HttpStatusCode? GetChangedStatusCodeForProperty(PropertyAccessPathSegment propertySegment)
+        {
+            IEdmTypeReference type = propertySegment.Property.Type;
+            return type.IsPrimitive() || type.IsComplex() ? HttpStatusCode.NoContent : (HttpStatusCode?)null;
+        }
+    }
+}
---
+++ test/System.Web.OData.Test/OData/ODataNullValueMessageHandlerTest.cs
@@ -0,0 +1,320 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing;
+using Microsoft.OData.Edm;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Web.OData.Test
+{
+    public class ODataNullValueMessageHandlerTest
+    {
+        [Fact]
+        public void SendAsync_ThrowsIfRequestIsNull()
+        {
+            // Arrange
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler();
+
+            // Act & Assert
+            Assert.ThrowsArgumentNull(() => { HttpResponseMessage result = handler.SendAsync(null).Result; }, "request");
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsNullIfResponseIsNull()
+        {
+            // Arrange
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+                {
+                    InnerHandler = new TestMessageHandler(null)
+                };
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Null(response);
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsOriginalResponseIfContentIsNull()
+        {
+            // Arrange
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsOriginalResponseIfNoObjectContent()
+        {
+            // Arrange
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            originalResponse.Content = new StringContent("test");
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsOriginalResponseIfObjectContentHasValue()
+        {
+            // Arrange
+            Mock<MediaTypeFormatter> formatter = new Mock<MediaTypeFormatter>();
+            formatter.Setup(f => f.CanWriteType(It.IsAny<Type>())).Returns(true);
+
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            originalResponse.Content = new ObjectContent(typeof(string), "value", formatter.Object);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.Created)]
+        [InlineData(HttpStatusCode.Redirect)]
+        [InlineData(HttpStatusCode.BadRequest)]
+        [InlineData(HttpStatusCode.InternalServerError)]
+        public void SendAsync_ReturnsOriginalResponseIfStatusCodeIsNotOk(HttpStatusCode status)
+        {
+            // Arrange
+            Mock<MediaTypeFormatter> formatter = new Mock<MediaTypeFormatter>();
+            formatter.Setup(f => f.CanWriteType(It.IsAny<Type>())).Returns(true);
+
+            HttpResponseMessage originalResponse = new HttpResponseMessage(status);
+            originalResponse.Content = new ObjectContent(typeof(string), null, formatter.Object);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Theory]
+        [InlineData("Delete")]
+        [InlineData("Post")]
+        [InlineData("Put")]
+        [InlineData("Patch")]
+        public void SendAsync_ReturnsOriginalResponseIfRequestIsNotGet(string method)
+        {
+            // Arrange
+            Mock<MediaTypeFormatter> formatter = new Mock<MediaTypeFormatter>();
+            formatter.Setup(f => f.CanWriteType(It.IsAny<Type>())).Returns(true);
+
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            originalResponse.Content = new ObjectContent(typeof(string), null, formatter.Object);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod(method), "http://localhost/any");
+            request.ODataProperties().Path = new ODataPath(new EntitySetPathSegment("EntitySet"));
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsOriginalResponseIfRequestDoesNotHaveODataPath()
+        {
+            // Arrange
+            Mock<MediaTypeFormatter> formatter = new Mock<MediaTypeFormatter>();
+            formatter.Setup(f => f.CanWriteType(It.IsAny<Type>())).Returns(true);
+
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            originalResponse.Content = new ObjectContent(typeof(string), null, formatter.Object);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Same(originalResponse, response);
+        }
+
+        [Fact]
+        public void SendAsync_ReturnsNotFoundForNullEntityResponse()
+        {
+            // Arrange
+            Mock<MediaTypeFormatter> formatter = new Mock<MediaTypeFormatter>();
+            formatter.Setup(f => f.CanWriteType(It.IsAny<Type>())).Returns(true);
+
+            HttpResponseMessage originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            originalResponse.Content = new ObjectContent(typeof(string), null, formatter.Object);
+
+            ODataNullValueMessageHandler handler = new ODataNullValueMessageHandler
+            {
+                InnerHandler = new TestMessageHandler(originalResponse)
+            };
+
+            ODataPath path = new DefaultODataPathHandler().Parse(BuildModel(), "http://localhost/any", "Customers(3)");
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "http://localhost/any");
+            request.SetConfiguration(new HttpConfiguration());
+            request.ODataProperties().Path = path;
+
+            // Act 
+            HttpResponseMessage response = handler.SendAsync(request).Result;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("Customers", null)]
+        [InlineData("Customers(3)", HttpStatusCode.NotFound)]
+        [InlineData("Customers(3)/Id", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)/Id/$value", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)/ComplexProperty", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)/PrimitiveCollection", null)]
+        [InlineData("Customers(3)/ComplexCollection", null)]
+        [InlineData("Customers(3)/NavigationProperty", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)/CollectionNavigationProperty", null)]
+        [InlineData("Customers(3)/CollectionNavigationProperty(3)", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)", HttpStatusCode.NotFound)]
+        [InlineData("Navigations/Test.SpecialNavigation", null)]
+        [InlineData("Navigations(3)/Test.SpecialNavigation", HttpStatusCode.NotFound)]
+        [InlineData("Navigations/Test.SpecialNavigation(3)", HttpStatusCode.NotFound)]
+        [InlineData("Navigations/Test.SpecialNavigation(3)/Test.Navigation", HttpStatusCode.NotFound)]
+        [InlineData("Customers(3)/NavigationProperty/Test.SpecialNavigation", HttpStatusCode.NoContent)]
+        [InlineData("Customers(3)/CollectionNavigationProperty/Test.SpecialNavigation(3)/Test.Navigation", HttpStatusCode.NoContent)]
+        [InlineData("BestNavigation", HttpStatusCode.NotFound)]
+        [InlineData("BestNavigation/Test.SpecialNavigation/Test.Navigation", HttpStatusCode.NotFound)]
+        public void GetResponseStatusCode_ReturnsNoContentForProperties_AndNotFoundForEntities(string odataPath,
+            HttpStatusCode? expected)
+        {
+            // Arrange
+            IEdmModel model = BuildModel();
+            IODataPathHandler pathHandler = new DefaultODataPathHandler();
+            ODataPath path = pathHandler.Parse(model, "http://localhost/any", odataPath);
+            // Guard
+            Assert.NotNull(path);
+
+            // Act
+            HttpStatusCode? statusCode = ODataNullValueMessageHandler.GetUpdatedResponseStatusCodeOrNull(path);
+
+            // Assert
+            Assert.Equal(expected, statusCode);
+        }
+
+        private static IEdmModel BuildModel()
+        {
+            var mb = new ODataConventionModelBuilder();
+            mb.EntitySet<Customer>("Customers");
+            mb.EntitySet<Navigation>("Navigations");
+            mb.Singleton<Navigation>("BestNavigation");
+            mb.EntityType<SpecialNavigation>().DerivesFrom<Navigation>();
+
+            // This is just a convenience for unit testing.
+            foreach (StructuralTypeConfiguration structuralType in mb.StructuralTypes)
+            {
+                structuralType.Namespace = "Test";
+            }
+
+            return mb.GetEdmModel();
+        }
+
+        public class Customer
+        {
+            public int Id { get; set; }
+            public Complex ComplexProperty { get; set; }
+            public ICollection<int> PrimitiveCollection { get; set; }
+            public ICollection<Complex> ComplexCollection { get; set; }
+            public Navigation NavigationProperty { get; set; }
+            public ICollection<Navigation> CollectionNavigationProperty { get; set; }
+        }
+
+        public class Navigation
+        {
+            public int Id { get; set; }
+        }
+
+        public class SpecialNavigation : Navigation
+        {
+        }
+
+        public class Complex
+        {
+            public int ComplexTypeProperty { get; set; }
+        }
+
+        private class TestMessageHandler : HttpMessageHandler
+        {
+            private readonly HttpResponseMessage _response;
+            public TestMessageHandler(HttpResponseMessage response)
+            {
+                _response = response;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(
+                HttpRequestMessage request,
+                CancellationToken cancellationToken)
+            {
+                return Task.FromResult(_response);
+            }
+        }
+    }
+}

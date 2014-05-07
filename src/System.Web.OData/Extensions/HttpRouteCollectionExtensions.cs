84c45e064df7ab8359f167daaed91664bef75ed0
jacalvar <jacalvar@microsoft.com>
2014-05-06 23:28:57 -0700

    Issue 1711 [Part2] - When returning an Entity that contains an ETag annotation, the ETag header should also be set. https://aspnetwebstack.codeplex.com/workitem/1711
    Created a specific MapODataServiceRoute overload to make registering
    per route HttpMessageHandlers in OData routes easier.

--- src/System.Web.OData/Extensions/HttpRouteCollectionExtensions.cs
+++ src/System.Web.OData/Extensions/HttpRouteCollectionExtensions.cs
@@ -3,7 +3,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using System.Web.OData.Batch;
 using System.Web.OData.Routing;
 using System.Web.OData.Routing.Conventions;
@@ -46,6 +48,23 @@ namespace System.Web.OData.Extensions
         {
             return MapODataServiceRoute(routes, routeName, routePrefix, model, new DefaultODataPathHandler(),
                 ODataRoutingConventions.CreateDefault(), batchHandler);
+        }
+
+        /// <summary>
+        /// Maps the specified OData route. When the <paramref name="defaultHandler"/> is provided, it will map it
+        /// as the default handler for the route.
+        /// </summary>
+        /// <param name="routes">A collection of routes for the application.</param>
+        /// <param name="routeName">The name of the route to map.</param>
+        /// <param name="routePrefix">The prefix to add to the OData route's path template.</param>
+        /// <param name="model">The EDM model to use for parsing OData paths.</param>
+        /// <param name="defaultHandler">The default <see cref="HttpMessageHandler"/> for this route.</param>
+        /// <returns>The added <see cref="ODataRoute"/>.</returns>
+        public static ODataRoute MapODataServiceRoute(this HttpRouteCollection routes, string routeName,
+            string routePrefix, IEdmModel model, HttpMessageHandler defaultHandler)
+        {
+            return MapODataServiceRoute(routes, routeName, routePrefix, model, new DefaultODataPathHandler(),
+                ODataRoutingConventions.CreateDefault(), defaultHandler);
         }
 
         /// <summary>
@@ -93,15 +112,7 @@ namespace System.Web.OData.Extensions
                 throw Error.ArgumentNull("routes");
             }
 
-            if (!String.IsNullOrEmpty(routePrefix))
-            {
-                int prefixLastIndex = routePrefix.Length - 1;
-                if (routePrefix[prefixLastIndex] == '/')
-                {
-                    // Remove the last trailing slash if it has one.
-                    routePrefix = routePrefix.Substring(0, routePrefix.Length - 1);
-                }
-            }
+            routePrefix = RemoveTrailingSlash(routePrefix);
 
             if (batchHandler != null)
             {
@@ -116,6 +127,74 @@ namespace System.Web.OData.Extensions
             ODataRoute route = new ODataRoute(routePrefix, routeConstraint);
             routes.Add(routeName, route);
             return route;
+        }
+
+        /// <summary>
+        /// Maps the specified OData route. When the <paramref name="defaultHandler"/> is provided, it will map it
+        /// as the handler for the route.
+        /// </summary>
+        /// <param name="routes">A collection of routes for the application.</param>
+        /// <param name="routeName">The name of the route to map.</param>
+        /// <param name="routePrefix">The prefix to add to the OData route's path template.</param>
+        /// <param name="model">The EDM model to use for parsing OData paths.</param>
+        /// <param name="pathHandler">The <see cref="IODataPathHandler" /> to use for parsing the OData path.</param>
+        /// <param name="routingConventions">
+        /// The OData routing conventions to use for controller and action selection.
+        /// </param>
+        /// <param name="defaultHandler">The default <see cref="HttpMessageHandler"/> for this route.</param>
+        /// <returns>The added <see cref="ODataRoute"/>.</returns>
+        public static ODataRoute MapODataServiceRoute(this HttpRouteCollection routes, string routeName,
+            string routePrefix, IEdmModel model, IODataPathHandler pathHandler,
+            IEnumerable<IODataRoutingConvention> routingConventions, HttpMessageHandler defaultHandler)
+        {
+            // We have a more specific overload to map batch handlers that creates a different route for the batch
+            // endpoint instead of mapping that handler as the per route handler. Given that HttpMessageHandler is a
+            // base type of ODataBatchHandler, it's possible the compiler will call this overload instead of the one
+            // for the batch handler, so we detect that case and call the appropiate overload for the user.
+            // The case in which the compiler picks the wrong overload is:
+            // HttpRequestMessageHandler batchHandler = new DefaultODataBatchHandler(httpServer);
+            // config.Routes.MapODataServiceRoute("routeName", "routePrefix", model, batchHandler);
+            if (defaultHandler != null)
+            {
+                ODataBatchHandler batchHandler = defaultHandler as ODataBatchHandler;
+                if (batchHandler != null)
+                {
+                    return MapODataServiceRoute(routes, routeName, routePrefix, model, batchHandler);
+                }
+            }
+
+            if (routes == null)
+            {
+                throw Error.ArgumentNull("routes");
+            }
+
+            routePrefix = RemoveTrailingSlash(routePrefix);
+
+            ODataPathRouteConstraint routeConstraint =
+                new ODataPathRouteConstraint(pathHandler, model, routeName, routingConventions);
+            ODataRoute route = new ODataRoute(
+                routePrefix,
+                routeConstraint,
+                defaults: null,
+                constraints: null,
+                dataTokens: null,
+                handler: defaultHandler);
+            routes.Add(routeName, route);
+            return route;
+        }
+
+        private static string RemoveTrailingSlash(string routePrefix)
+        {
+            if (!String.IsNullOrEmpty(routePrefix))
+            {
+                int prefixLastIndex = routePrefix.Length - 1;
+                if (routePrefix[prefixLastIndex] == '/')
+                {
+                    // Remove the last trailing slash if it has one.
+                    routePrefix = routePrefix.Substring(0, routePrefix.Length - 1);
+                }
+            }
+            return routePrefix;
         }
     }
 }

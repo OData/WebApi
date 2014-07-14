20ee81efd6beb1c3c9ee09cee2051d6d594fcdce
Pranav K <prkrishn@hotmail.com>
2014-07-14 16:05:20 -0700

    Stylecop fixes

--- src/System.Web.WebPages/StringWriterExtensions.cs
+++ src/System.Web.WebPages/StringWriterExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.IO;
 using System.Text;
 
 namespace System.Web.WebPages

33d4c6e93fc8795be71822b247ce57788e9327f6
Sam Xu <saxu@microsoft.com>
2015-02-17 09:38:25 +0800

    DateTime doesn't work for ETag. #224
    After #221 checked in, this issue is fixed. Add one test cases to verify.
    Thanks.

--- OData/test/System.Web.OData.Test/OData/Formatter/DefaultODataETagHandlerTests.cs
+++ OData/test/System.Web.OData.Test/OData/Formatter/DefaultODataETagHandlerTests.cs
@@ -48,6 +48,32 @@ namespace System.Web.OData.Formatter
         }
 
         [Theory]
+        [InlineData("UTC")] // +0:00
+        [InlineData("Pacific Standard Time")] // -8:00
+        [InlineData("China Standard Time")] // +8:00
+        public void DefaultODataETagHandler_DateTime_RoundTrips(string timeZoneId)
+        {
+            // Arrange
+            TimeZoneInfoHelper.TimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            DateTime value = new DateTime(2015, 2, 17, 1, 2, 3, DateTimeKind.Utc);
+
+            DefaultODataETagHandler handler = new DefaultODataETagHandler();
+            Dictionary<string, object> properties = new Dictionary<string, object> { { "Any", value } };
+
+            // Act
+            EntityTagHeaderValue etagHeaderValue = handler.CreateETag(properties);
+            IList<object> values = handler.ParseETag(etagHeaderValue).Select(p => p.Value).ToList();
+
+            // Assert
+            Assert.True(etagHeaderValue.IsWeak);
+            Assert.Equal(1, values.Count);
+            DateTimeOffset result = Assert.IsType<DateTimeOffset>(values[0]);
+
+            Assert.Equal(new DateTimeOffset(value.ToUniversalTime()).ToOffset(TimeZoneInfoHelper.TimeZone.BaseUtcOffset),
+                result);
+        }
+
+        [Theory]
         [InlineData("1", new object[] { "any", 1 })]
         public void CreateETag_ETagCreatedAndParsed_GivenValues(string notUsed, object[] values)
         {

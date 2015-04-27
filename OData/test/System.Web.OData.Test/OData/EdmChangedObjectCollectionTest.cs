404354a8f52e7da9614f726a8e9baf982294d14a
Sagar Hotchandani <sagarho@microsoft.com>
2015-04-27 17:20:48 +0530

    DeltaFeed Serialization in WebAPI
    Added support for DeltaFeed Serialization in WebAPI

---
+++ OData/test/System.Web.OData.Test/OData/EdmChangedObjectCollectionTest.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Library;
+using Microsoft.TestCommon;
+using Moq;
+
+namespace System.Web.OData
+{
+     public class EdmChangedObjectCollectionTest
+    {
+         [Fact]
+         public void Ctor_ThrowsArgumentNull_EdmType()
+         {
+             Assert.ThrowsArgumentNull(() => new EdmChangedObjectCollection(entityType: null), "entityType");
+         }
+
+         [Fact]
+         public void Ctor_ThrowsArgumentNull_List()
+         {
+             IEdmEntityType entityType = new Mock<IEdmEntityType>().Object;
+             Assert.ThrowsArgumentNull(() => new EdmChangedObjectCollection(entityType, changedObjectList: null), "list");
+         }
+
+         [Fact]
+         public void GetEdmType_Returns_EdmTypeInitializedByCtor()
+         {
+             IEdmEntityType _entityType = new EdmEntityType("NS", "Entity");
+             var edmObject = new EdmChangedObjectCollection(_entityType);
+             IEdmCollectionTypeReference collectionTypeReference = (IEdmCollectionTypeReference)edmObject.GetEdmType();
+
+             Assert.Same(_entityType, collectionTypeReference.ElementType().Definition);
+         }
+   }
+}

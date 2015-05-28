ae3a3ac7fbc4fccdf81d5168ac480cfe492be406
Lewis Cheng <licheng@microsoft.com>
2015-05-27 23:43:06 -0700

    Add EnableQueryAttribute and support $filter

---
+++ vNext/src/Microsoft.AspNet.OData/EnableQueryAttribute.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Common;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNet.OData.Routing;
+
+namespace Microsoft.AspNet.OData
+{
+    // TODO: Replace with full version in the future.
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class EnableQueryAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context == null)
+            {
+                throw Error.ArgumentNull("context");
+            }
+
+            var response = context.HttpContext.Response;
+            if (!response.IsSuccessStatusCode())
+            {
+                return;
+            }
+
+            var result = context.Result as ObjectResult;
+            if (result == null)
+            {
+                throw Error.Argument("context", SRResources.QueryingRequiresObjectContent, context.Result.GetType().FullName);
+            }
+
+            var request = context.HttpContext.Request;
+            if (result.Value != null && request.HasQueryOptions())
+            {
+                result.Value = ApplyQueryOptions(result.Value, request, context.ActionDescriptor);
+            }
+        }
+
+        public virtual object ApplyQueryOptions(object value, HttpRequest request, ActionDescriptor descriptor)
+        {
+            var elementClrType = TypeHelper.GetImplementedIEnumerableType(value.GetType());
+
+            var model = request.ODataProperties().Model;
+            if (model == null)
+            {
+                throw Error.InvalidOperation(SRResources.QueryGetModelMustNotReturnNull);
+            }
+
+            var queryContext = new ODataQueryContext(
+                model,
+                elementClrType,
+                request.ODataProperties().Path);
+
+            var queryOptions = new ODataQueryOptions(queryContext, request);
+
+            var enumerable = value as IEnumerable;
+            if (enumerable == null)
+            {
+                // response is single entity.
+                return value;
+            }
+
+            // response is a collection.
+            var query = (value as IQueryable) ?? enumerable.AsQueryable();
+            return queryOptions.ApplyTo(query,
+                new ODataQuerySettings
+                {
+                    HandleNullPropagation = HandleNullPropagationOption.True
+                });
+        }
+    }
+}
---
+++ vNext/src/Microsoft.AspNet.OData/Query/Expressions/LinqParameterContainer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Microsoft.AspNet.OData.Query.Expressions
+{
+    // wraps a constant value so that EntityFramework paramterizes the constant.
+    internal abstract class LinqParameterContainer
+    {
+        private static ConcurrentDictionary<Type, Func<object, LinqParameterContainer>> _ctors = new ConcurrentDictionary<Type, Func<object, LinqParameterContainer>>();
+
+        // the value of the constant.
+        public abstract object Property { get; }
+
+        public static Expression Parameterize(Type type, object value)
+        {
+            // () => new LinqParameterContainer(constant).Property
+            // instead of returning a constant expression node, wrap that constant in a class the way compiler 
+            // does a closure, so that EF can parameterize the constant (resulting in better performance due to expression translation caching).
+            LinqParameterContainer containedValue = LinqParameterContainer.Create(type, value);
+            return Expression.Property(Expression.Constant(containedValue), "TypedProperty");
+        }
+
+        private static LinqParameterContainer Create(Type type, object value)
+        {
+            return _ctors.GetOrAdd(type, t =>
+            {
+                MethodInfo createMethod = typeof(LinqParameterContainer).GetMethod("CreateInternal").MakeGenericMethod(t);
+                ParameterExpression valueParameter = Expression.Parameter(typeof(object));
+                return
+                    Expression.Lambda<Func<object, LinqParameterContainer>>(
+                        Expression.Call(
+                            createMethod,
+                            Expression.Convert(valueParameter, t)),
+                        valueParameter)
+                    .Compile();
+            })(value);
+        }
+
+        // invoked dynamically at runtime.
+        public static LinqParameterContainer CreateInternal<T>(T value)
+        {
+            return new TypedLinqParameterContainer<T>(value);
+        }
+
+        // having a strongly typed property avoids the a cast in the property access expression that would be 
+        // generated for this constant.
+        internal class TypedLinqParameterContainer<T> : LinqParameterContainer
+        {
+            public TypedLinqParameterContainer(T value)
+            {
+                TypedProperty = value;
+            }
+
+            public T TypedProperty { get; set; }
+
+            public override object Property
+            {
+                get { return TypedProperty; }
+            }
+        }
+    }
+}

4e3f1761d2e8badb5debee4e4d3bca63be08ce8d
Bruce Johnston <bruce.johnston@microsoft.com>
2015-06-07 17:00:38 -0700

    Making ODataQueryOptions.GetNextPageLink public. This will be useful for anyone processing OData query options directly (i.e. -- not using LINQ and ApplyTo()).

--- OData/src/System.Web.OData/OData/Query/ODataQueryOptions.cs
+++ OData/src/System.Web.OData/OData/Query/ODataQueryOptions.cs
@@ -508,7 +508,13 @@ namespace System.Web.OData.Query
             return truncatedCollection.AsQueryable();
         }
 
-        internal static Uri GetNextPageLink(HttpRequestMessage request, int pageSize)
+        /// <summary>
+        /// Creates a link for the next page of results; To be used as the value of @odata.nextLink.
+        /// </summary>
+        /// <param name="request">The request on which to base the next page link.</param>
+        /// <param name="pageSize">The number of results allowed per page.</param>
+        /// <returns>A next page link.</returns>
+        public static Uri GetNextPageLink(HttpRequestMessage request, int pageSize)
         {
             Contract.Assert(request != null);
             Contract.Assert(request.RequestUri != null);

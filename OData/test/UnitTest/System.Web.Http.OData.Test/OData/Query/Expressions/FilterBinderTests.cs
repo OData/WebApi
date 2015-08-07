2a3ec6da24e6d03bb02fabb330f681e7a3bd932e
Ilya Chernoudov <ilchertoz@gmail.com>
2015-08-07 16:43:40 +0300

    String function test with null propagation added.

--- OData/test/UnitTest/System.Web.Http.OData.Test/OData/Query/Expressions/FilterBinderTests.cs
+++ OData/test/UnitTest/System.Web.Http.OData.Test/OData/Query/Expressions/FilterBinderTests.cs
@@ -408,6 +408,18 @@ namespace System.Web.Http.OData.Query.Expressions
                 new Product { UnitsInStock = ToNullable<short>(unitsInStock) },
                 new { WithNullPropagation = withNullPropagation, WithoutNullPropagation = withoutNullPropagation });
         }
+
+
+        [Theory]
+        [InlineData("Abcd", true, true)]
+        public void NullHandling_StringFunctionWithStringParameret(string productName, bool withNullPropagation, object withoutNullPropagation)
+        {
+            var filters = VerifyQueryDeserialization(
+                "startswith(ProductName, 'Abc')",
+                NotTesting,
+                "$it => (IIF((($it.ProductName == null) OrElse (\"Abc\" == null)), null, Convert($it.ProductName.StartsWith(\"Abc\"))) == True)");
+        }
+
         #endregion
 
         [Theory]

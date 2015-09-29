0b1f8a7db7e56c3ae1fe22ccd1519860a70b5910
Sam Xu <saxu@microsoft.com>
2015-09-29 13:31:40 +0800

    Add swagger model APIs. Merge PR #520. Thanks.

---
+++ OData/test/E2ETest/WebStack.QA.Test.OData/Swagger/SwaggerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Extensions;
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Csdl;
+using Newtonsoft.Json.Linq;
+
+namespace WebStack.QA.Test.OData.Swagger
+{
+    public class SwaggerController : ApiController
+    {
+        private static readonly Version _defaultEdmxVersion = new Version(4, 0);
+
+        [EnableQuery]
+        public JObject GetSwagger()
+        {
+            IEdmModel model = Request.ODataProperties().Model;
+            model.SetEdmxVersion(_defaultEdmxVersion);
+            ODataSwaggerConverter converter = new ODataSwaggerConverter(model);
+            return converter.ConvertToSwaggerModel();
+        }
+    }
+}
---
+++ OData/test/E2ETest/WebStack.QA.Test.OData/Swagger/SwaggerPathSegment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.OData.Routing;
+using Microsoft.OData.Edm;
+
+namespace WebStack.QA.Test.OData.Swagger
+{
+    public class SwaggerPathSegment : ODataPathSegment
+    {
+        /// <inheritdoc/>
+        public override string SegmentKind
+        {
+            get
+            {
+                return "$swagger";
+            }
+        }
+
+        /// <inheritdoc/>
+        public override IEdmType GetEdmType(IEdmType previousEdmType)
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public override IEdmNavigationSource GetNavigationSource(IEdmNavigationSource previousNavigationSource)
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return "$swagger";
+        }
+
+        /// <inheritdoc/>
+        public override bool TryMatch(ODataPathSegment pathSegment, IDictionary<string, object> values)
+        {
+            return pathSegment.SegmentKind == "$swagger" || pathSegment.SegmentKind == "swagger.json";
+        }
+    }
+}

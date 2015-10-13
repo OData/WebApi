e935b94cbd12986e4e7c6f8fc7a3c04296253be8
Evan Machusak <evanmachusak@hotmail.com>
2015-10-13 16:00:17 -0400

    Edited build.cmd

--- build.cmd
+++ build.cmd
@@ -14,10 +14,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild vNext\WebApiOData.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%MSBuild% vNext\WebApiOData.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
-%nuget% pack "vNext/Microsoft.AspNet.OData.6.0.0-beta8.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "vNext\Microsoft.AspNet.OData.6.0.0-beta8.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"

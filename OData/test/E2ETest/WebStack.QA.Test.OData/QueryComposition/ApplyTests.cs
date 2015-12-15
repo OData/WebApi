d2202aba649f7948f7019c77467db5c545d5d4d7
Konstantin Kosinsky <kokosinsky@hotmail.com>
2015-12-15 09:12:13 -0800

    Initial E2E tests for $apply

---
+++ OData/test/E2ETest/WebStack.QA.Test.OData/QueryComposition/ApplyTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStack.QA.Test.OData.Common;
+using Xunit;
+
+namespace WebStack.QA.Test.OData.QueryComposition
+{
+    public class ApplyTests : ODataTestBase
+    {
+        [Fact]
+        public void CanApply()
+        {
+            var response = this.Client.GetAsync(this.BaseAddress + "/api/ApplyTests/Get?$apply=groupby((Name))").Result;
+            var result = response.Content.ReadAsAsync<IEnumerable<dynamic>>().Result;
+
+        }
+    }
+}
---
+++ OData/test/E2ETest/WebStack.QA.Test.OData/QueryComposition/Controllers/ApplyTestsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using WebStack.QA.Test.OData.Cast;
+
+namespace WebStack.QA.Test.OData.QueryComposition.Controllers
+{
+    public class ApplyTestsController : ApiController
+    {
+        [EnableQuery]
+        public IQueryable Get()
+        {
+            return DataSource.EfProducts;
+        }
+    }
+}

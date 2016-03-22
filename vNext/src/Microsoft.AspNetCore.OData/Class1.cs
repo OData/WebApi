66bd9002e49a342baebfa390d45b24efcfc74f7c
Josh Comley <Josh Comley>
2016-03-22 18:00:35 +0100

    Updated all references Moved to dnxcore50

--- vNext/src/Microsoft.AspNetCore.OData/Class1.cs
+++ vNext/src/Microsoft.AspNetCore.OData/Class1.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using Microsoft.OData.Edm;
-using System;
 
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Framework.Internal
 {
 	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]

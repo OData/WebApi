cb8b11a43048145a75c3b7049fa4dcc5f259df8b
Josh Comley <joshcomley@googlemail.com>
2016-04-08 04:46:36 +0100

    Formatting

--- vNext/src/Microsoft.AspNetCore.OData/Extensions/PropertyInfoExtensions.cs
+++ vNext/src/Microsoft.AspNetCore.OData/Extensions/PropertyInfoExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.OData.Builder;

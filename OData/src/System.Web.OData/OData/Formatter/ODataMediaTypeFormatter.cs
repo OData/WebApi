92464ddcdce6ef526d62e7fefc7b09fbdb81ec62
Jack Freelander <jackfree@microsoft.com>
2016-04-18 16:38:31 -0700

    Preserve BaseAddressFactory in copy constructor

--- OData/src/System.Web.OData/OData/Formatter/ODataMediaTypeFormatter.cs
+++ OData/src/System.Web.OData/OData/Formatter/ODataMediaTypeFormatter.cs
@@ -124,6 +124,7 @@ namespace System.Web.OData.Formatter
             _payloadKinds = formatter._payloadKinds;
             MessageWriterSettings = formatter.MessageWriterSettings;
             MessageReaderSettings = formatter.MessageReaderSettings;
+            BaseAddressFactory = formatter.BaseAddressFactory;
 
             // Parameter 2: version
             _version = version;

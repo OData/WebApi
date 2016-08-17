315271719a6e741a1bfeebca4ba62f5f764c7673
Lewis Cheng <licheng@microsoft.com>
2016-08-17 15:18:17 +0800

    Remove unnecessary using

--- OData/src/System.Web.OData/Extensions/HttpRequestMessageExtensions.cs
+++ OData/src/System.Web.OData/Extensions/HttpRequestMessageExtensions.cs
@@ -11,7 +11,6 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Web.Http;
 using System.Web.OData.Formatter;

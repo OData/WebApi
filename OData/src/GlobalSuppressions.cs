0ee4550529e3f7f25a1b5daec74185e097eab8c1
Sam Xu <saxu@microsoft.com>
2016-09-19 19:30:57 -0700

    Remove the OData folder

--- OData/src/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
-// Licensed under the MIT License.  See License.txt in the project root for license information.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "Assembly is delay-signed")]

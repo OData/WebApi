176f5d4f1f0cc752f0280a96fd6452b9de5f77c8
mziyabo <mziyabo@live.com>
2016-11-03 17:57:48 +0200

    Added OData .NET Core Web API Sample

---
+++ vNext/samples/ODataSample.NETCore/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ODataSample.Web.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}

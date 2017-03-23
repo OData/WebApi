fe468cd5fd0c4705184cc38cb56dc0f96d657b03
Michael Pizzo <mikepizzo@hotmail.com>
2017-03-23 16:22:34 -0700

    Reorder using statements in EdmModelHelperMethods.cs.

--- OData/src/System.Web.OData/OData/Builder/EdmModelHelperMethods.cs
+++ OData/src/System.Web.OData/OData/Builder/EdmModelHelperMethods.cs
@@ -1,6 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Csdl;
+using Microsoft.OData.Edm.Validation;
+using Microsoft.OData.Edm.Vocabularies;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Contracts;
@@ -10,10 +14,6 @@ using System.Text;
 using System.Web.Http;
 using System.Web.OData.Properties;
 using System.Web.OData.Query;
-using Microsoft.OData.Edm;
-using Microsoft.OData.Edm.Validation;
-using Microsoft.OData.Edm.Vocabularies;
-using Microsoft.OData.Edm.Csdl;
 
 namespace System.Web.OData.Builder
 {

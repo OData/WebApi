0c660a7ae6d44794b64339c3dd7dcbc2dd03dfeb
mtroth-microsoft <mtroth@microsoft.com>
2017-05-25 09:52:49 -0700

    Add comments from feedback.

--- OData/test/UnitTest/System.Web.OData.Test/OData/Query/EnableQueryAttributeTest.cs
+++ OData/test/UnitTest/System.Web.OData.Test/OData/Query/EnableQueryAttributeTest.cs
@@ -1054,12 +1054,15 @@ namespace System.Web.OData.Query
         [Fact]
         public void OnActionExecuted_StringValue()
         {
+            // Arrange
             string stringResult = "foo";
             EnableQueryAttribute attribute = new EnableQueryAttribute();
             HttpActionExecutedContext actionExecutedContext = GetActionExecutedContext("http://localhost/Suppliers(1)/CompanyName?customqueryoption=bar", stringResult);
 
+            // Act
             attribute.OnActionExecuted(actionExecutedContext);
 
+            // Assert
             Assert.Equal(HttpStatusCode.OK, actionExecutedContext.Response.StatusCode);
             Assert.Equal(stringResult, ((ObjectContent)actionExecutedContext.Response.Content).Value);
         }
@@ -1067,12 +1070,15 @@ namespace System.Web.OData.Query
         [Fact]
         public void OnActionExecuted_ByteArrayValue()
         {
+            // Arrange
             byte[] bytesResult = BitConverter.GetBytes(42);
             EnableQueryAttribute attribute = new EnableQueryAttribute();
             HttpActionExecutedContext actionExecutedContext = GetActionExecutedContext("http://localhost/Suppliers(1)/Version?customqueryoption=bar", bytesResult);
 
+            // Act
             attribute.OnActionExecuted(actionExecutedContext);
 
+            // Assert
             Assert.Equal(HttpStatusCode.OK, actionExecutedContext.Response.StatusCode);
             Assert.Equal(bytesResult, ((ObjectContent)actionExecutedContext.Response.Content).Value);
         }

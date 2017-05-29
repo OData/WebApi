8caebf9043caa35ee0be52f35e28b2d4be1d44a3
Biao Li <biaol@microsoft.com>
2017-05-29 12:22:09 -0700

    V4-5.x branch: Remove ODataV3 src and tests from build and slns.

--- test/E2ETestV3/Nuwa.WebStack/NuwaWebDeploymentConfigurationAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Nuwa
-{
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public class NuwaWebDeploymentConfigurationAttribute : Attribute
-    {
-    }
-}

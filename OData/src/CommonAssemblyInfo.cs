4805021c8e6bfcdf99c86f29902fb39a22e4759c
Konstantin Kosinsky <kokosins@microsoft.com>
2017-06-08 08:54:16 -0700

    Version update

--- OData/src/CommonAssemblyInfo.cs
+++ OData/src/CommonAssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 
 #if ASPNETODATA
 #if !BUILD_GENERATED_VERSION
-[assembly: AssemblyVersion("6.0.1.0")] // ASPNETODATA
-[assembly: AssemblyFileVersion("6.0.1.0")] // ASPNETODATA
+[assembly: AssemblyVersion("6.0.1.2")] // ASPNETODATA
+[assembly: AssemblyFileVersion("6.0.1.2")] // ASPNETODATA
 #endif
 [assembly: AssemblyProduct("Microsoft OData Web API")]
 #endif

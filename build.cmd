7d3341695c3e24f7bfdbbfc35ef2533a87f3cbfd
Rob Ward <robward@microsoft.com>
2017-06-12 12:11:58 -0700

    By default, make build.cmd run all tests as to ensure nothing is missed. Optionally, run build.cmd quick to run only the unit tests.

--- build.cmd
+++ build.cmd
@@ -12,21 +12,45 @@ REM is not supported. Also handle x86 operating systems, where %ProgramFiles(x86
 REM %MSBuild% value when setting the variable and never quote %MSBuild% references.
 set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+set FullBuild=0
 
-if /I "%1" == "" goto BuildDefaults
+REM with no switches, run the full set of tests.
+if /I "%1" == "" (
+  set FullBuild=1
+  goto BuildDefaults
+)
+
+REM support quick build, unit tests only.
+if /I "%1" == "QUICK" (
+  set FullBuild=0
+  goto BuildDefaults
+)
+if /I "%1" == "-Q" (
+  set FullBuild=0
+  goto BuildDefaults
+)
+
+REM Continue to suport original switches for those
+REM who might have scripts setup to use them.
 if /I "%1" == "E2EV4" goto BuildE2EV4
-if /I "%1" == "FULL" goto BuildDefaults
+if /I "%1" == "FULL" (
+  set FullBuild=1
+  goto BuildDefaults
+)
 
+REM Build a specified target
 %MSBuild% WebApiOData.msbuild /m /nr:false /t:%* /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if %ERRORLEVEL% neq 0 goto BuildFail
 goto BuildSuccess
 
+REM Build product code and unit tests
 :BuildDefaults
 %MSBuild% WebApiOData.msbuild /m /nr:false /p:Platform="Any CPU" /p:Desktop=true /v:M /fl /flp:LogFile=bin\msbuild.log;Verbosity=Normal
 if %ERRORLEVEL% neq 0 goto BuildFail
-if /I "%1" == "FULL" goto BuildE2EV4
+if %FullBuild% neq 0 goto BuildE2EV4
 goto BuildSuccess
 
+REM Build product and V4 End to End tests
 :BuildE2EV4
 echo *** E2EV4 Test against KatanaSelf ***
 PowerShell.exe -executionpolicy remotesigned -File tools\scripts\ReplaceAppConfigValue.ps1 test\E2ETest\WebStack.QA.Test.OData\App.config "Nuwa.DefaultHostTypes" "KatanaSelf"

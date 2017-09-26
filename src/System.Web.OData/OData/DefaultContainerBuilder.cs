6582bf6fe08e03a67253759d88825e5e88e4003e
ysmoradi <ysmoradi@outlook.com>
2017-09-26 12:32:30 +0330

    Fixed namespaces sort in default container builder file

--- src/System.Web.OData/OData/DefaultContainerBuilder.cs
+++ src/System.Web.OData/OData/DefaultContainerBuilder.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+using System.Reflection;
 using System.Web.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OData;
 using ServiceLifetime = Microsoft.OData.ServiceLifetime;
-using System.Reflection;
 
 namespace System.Web.OData
 {

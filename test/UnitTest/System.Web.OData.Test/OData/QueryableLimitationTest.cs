c12bebb5079fbd298dc13074ab9dd2d4bb27d97e
Lucas Loegel <lucas.loegel@enablon.com>
2017-10-06 09:26:24 +0200

    Sort using

--- test/UnitTest/System.Web.OData.Test/OData/QueryableLimitationTest.cs
+++ test/UnitTest/System.Web.OData.Test/OData/QueryableLimitationTest.cs
@@ -6,13 +6,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.OData.Builder;
 using System.Web.OData.Extensions;
 using System.Web.OData.Query;
 using Microsoft.OData.Edm;
 using Microsoft.TestCommon;
-using System.Threading.Tasks;
 
 namespace System.Web.OData
 {

10262194cd983672ea90538f928d378454c4c2c3
Anton Tykhyy <atykhyy@gmail.com>
2018-01-08 20:29:28 +0200

    [feature/netcore] HttpRequest(Message) in OData container (#1143)
    Bind HttpRequest(Message) into per-request OData container

---
+++ src/Microsoft.AspNet.OData/HttpRequestScope.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Net.Http;
+
+namespace Microsoft.AspNet.OData
+{
+    /// <summary>
+    /// Provides access to the <see cref="HttpRequestMessage"/>
+    /// to which the OData service container instance is scoped.
+    /// </summary>
+    public class HttpRequestScope
+    {
+        /// <summary>
+        /// Provides access to the <see cref="HttpRequestMessage"/>
+        /// to which the OData service container instance is scoped.
+        /// </summary>
+        public HttpRequestMessage HttpRequest { get; set; }
+    }
+}
---
+++ src/Microsoft.AspNetCore.OData/HttpRequestScope.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNet.OData
+{
+    /// <summary>
+    /// Provides access to the <see cref="HttpRequest"/>
+    /// to which the OData service container instance is scoped.
+    /// </summary>
+    public class HttpRequestScope
+    {
+        /// <summary>
+        /// Provides access to the <see cref="HttpRequest"/>
+        /// to which the OData service container instance is scoped.
+        /// </summary>
+        public HttpRequest HttpRequest { get; set; }
+    }
+}

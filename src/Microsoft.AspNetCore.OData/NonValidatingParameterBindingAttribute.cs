36af7a65b262e3873123a9d5299c948c9510bd89
Sam Xu <saxu@microsoft.com>
2018-03-25 14:03:42 -0700

    Fix the enable the Delta with changed property

--- src/Microsoft.AspNetCore.OData/NonValidatingParameterBindingAttribute.cs
+++ src/Microsoft.AspNetCore.OData/NonValidatingParameterBindingAttribute.cs
@@ -3,6 +3,8 @@
 
 using System;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Microsoft.AspNet.OData
 {
@@ -13,12 +15,21 @@ namespace Microsoft.AspNet.OData
     /// This is essentially a <see cref="ValidateNeverAttribute"/>.
     /// </remarks>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
-    internal sealed partial class NonValidatingParameterBindingAttribute : Attribute, IPropertyValidationFilter
+    internal sealed partial class NonValidatingParameterBindingAttribute : ModelBinderAttribute, IPropertyValidationFilter
     {
         /// <inheritdoc />
         public bool ShouldValidateEntry(ValidationEntry entry, ValidationEntry parentEntry)
         {
             return false;
+        }
+
+        /// <inheritdoc/>
+        public override BindingSource BindingSource
+        {
+            get
+            {
+                return BindingSource.Body;
+            }
         }
     }
 }

32d620e207b89edb207a6e7baaaa2ea2a1b10b39
Sam Xu <saxu@microsoft.com>
2018-03-27 13:32:36 -0700

    Add a sample project for Asp.NET Core OData

---
+++ samples/AspNetCoreODataSample.Web/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using AspNetCoreODataSample.Web.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreODataSample.Web.Controllers
+{
+    public class MoviesController : ODataController
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+
+            if (_context.Movies.Count() == 0)
+            {
+                Movie m = new Movie
+                {
+                    Title = "Conan",
+                    ReleaseDate = new DateTimeOffset(new DateTime(2017, 3, 3)),
+                    Genre = Genre.Comedy,
+                    Price = 1.99m
+                };
+                _context.Movies.Add(m);
+                _context.SaveChanges();
+            }
+        }
+
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_context.Movies);
+        }
+
+        [EnableQuery]
+        public IActionResult Get(int key)
+        {
+            Movie m = _context.Movies.FirstOrDefault(c => c.ID == key);
+            if (m == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(m);
+        }
+    }
+}
---
+++ samples/AspNetCoreODataSample.Web/Models/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreODataSample.Web.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}

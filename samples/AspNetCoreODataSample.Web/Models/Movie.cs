4b0a7f669082ad29f3749d36089fdd27e8853cd5
Sam Xu <saxu@microsoft.com>
2018-03-27 13:32:36 -0700

    Add a sample project for Asp.NET Core OData

---
+++ samples/AspNetCoreODataSample.Web/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+
+namespace AspNetCoreODataSample.Web.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTimeOffset ReleaseDate { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ samples/AspNetCoreODataSample.Web/Startup.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreODataSample.Web.Models;
+
+namespace AspNetCoreODataSample.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MovieContext>(opt => opt.UseInMemoryDatabase("MovieList"));
+            services.AddOData();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(builder => builder.MapODataServiceRoute("odata", "odata", EdmModelBuilder.GetEdmModel()));
+        }
+    }
+}

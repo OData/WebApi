f7729e2681a84d2a587fdf366403a360a6efeb58
Sam Xu <saxu@microsoft.com>
2018-04-12 13:47:30 -0700

    Separate TestController to differnt abstraction

---
+++ test/UnitTest/Microsoft.Test.AspNet.Common/AspNetCore/TestNonODataController.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.Test.AspNet.OData
+{
+    /// <summary>
+    /// NonODataController is an abstracted ASP.NET [Core] controller.
+    /// </summary>
+    public class TestNonODataController : Controller
+    {
+    }
+}

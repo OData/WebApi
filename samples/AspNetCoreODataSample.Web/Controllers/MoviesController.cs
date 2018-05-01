5e0119fd3a05c6a3c87f63b65fc7082dfec8867c
Sam Xu <saxu@microsoft.com>
2018-05-01 15:10:53 -0700

    modify OData sample to verify issue #1413

--- samples/AspNetCoreODataSample.Web/Controllers/MoviesController.cs
+++ samples/AspNetCoreODataSample.Web/Controllers/MoviesController.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AspNetCoreODataSample.Web.Models;
 using Microsoft.AspNet.OData;
@@ -12,6 +13,8 @@ namespace AspNetCoreODataSample.Web.Controllers
     public class MoviesController : ODataController
     {
         private readonly MovieContext _context;
+
+        private readonly IList<Movie> _inMemoryMovies;
 
         public MoviesController(MovieContext context)
         {
@@ -29,18 +32,54 @@ namespace AspNetCoreODataSample.Web.Controllers
                 _context.Movies.Add(m);
                 _context.SaveChanges();
             }
+
+            _inMemoryMovies = new List<Movie>
+            {
+                new Movie
+                {
+                    ID = 1,
+                    Title = "Conan",
+                    ReleaseDate = new DateTimeOffset(new DateTime(2018, 3, 3)),
+                    Genre = Genre.Comedy,
+                    Price = 1.99m
+                },
+                new Movie
+                {
+                    ID = 2,
+                    Title = "James",
+                    ReleaseDate = new DateTimeOffset(new DateTime(2017, 3, 3)),
+                    Genre = Genre.Adult,
+                    Price = 91.99m
+                }
+            };
         }
 
         [EnableQuery]
         public IActionResult Get()
         {
-            return Ok(_context.Movies);
+            if (Request.Path.Value.Contains("efcore"))
+            {
+                return Ok(_context.Movies);
+            }
+            else
+            {
+                return Ok(_inMemoryMovies);
+            }
         }
 
         [EnableQuery]
         public IActionResult Get(int key)
         {
-            Movie m = _context.Movies.FirstOrDefault(c => c.ID == key);
+            Movie m;
+            if (Request.Path.Value.Contains("efcore"))
+            {
+                m = _context.Movies.FirstOrDefault(c => c.ID == key);
+            }
+            else
+            {
+                m = _inMemoryMovies.FirstOrDefault(c => c.ID == key);
+            }
+
             if (m == null)
             {
                 return NotFound();

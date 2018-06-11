dde1df821c5949fb24cc1fea2e1ab793944f6d96
Bruce Johnston <bruce.johnston@microsoft.com>
2018-06-10 21:17:59 -0700

    Merge PR#1458: Allow recursive comple3x types in OData model builder

--- test/UnitTest/Microsoft.AspNet.OData.Test.Shared/Builder/TestModels/RecursiveModel.cs
+++ test/UnitTest/Microsoft.AspNet.OData.Test.Shared/Builder/TestModels/RecursiveModel.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Microsoft.AspNet.OData.Test.Builder.TestModels
 {
@@ -19,5 +20,54 @@ namespace Microsoft.AspNet.OData.Test.Builder.TestModels
     public class MyExpression : GenericSurrogate
     {
         public GenericSurrogate Item { get; set; }
+    }
+
+    public class Parent
+    {
+        public Child OnlyChild { get; set; }
+    }
+
+    public class Child : Parent
+    {
+        public Parent FavoriteParent { get; set; }
+
+        public Car Car { get; set; }
+    }
+
+    public class Person
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public Parent ResponsibleGuardian { get; set; }
+    }
+
+    public class CountryDetails
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public class RecursiveAddress
+    {
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        // Navigation property
+        public CountryDetails Country { get; set; }
+
+        // Recursive reference
+        public RecursiveAddress PreviousAddress { get; set; }
+    }
+
+    public class Organization
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public RecursiveAddress HeadquartersAddress { get; set; }
     }
 }

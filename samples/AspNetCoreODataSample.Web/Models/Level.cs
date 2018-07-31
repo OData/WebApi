5a4d7f3c8b4369381ea1521fdd32df1227b4437c
Sam Xu <saxu@microsoft.com>
2018-07-31 12:01:39 -0700

    Fix the issue #850, (de)serialize using the Edm Enum member string

---
+++ samples/AspNetCoreODataSample.Web/Models/Level.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Runtime.Serialization;
+
+namespace AspNetCoreODataSample.Web.Models
+{
+    [DataContract(Name = "level")]
+    public enum Level
+    {
+        [EnumMember(Value = "low")]
+        Low,
+
+        [EnumMember(Value = "medium")]
+        Medium,
+
+        [EnumMember(Value = "high")]
+        High
+    }
+}

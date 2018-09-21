d9b1721c83fca788e1545a9ca2959a8277a5760a
KanishManuja-MS <41647051+KanishManuja-MS@users.noreply.github.com>
2018-09-21 13:59:58 -0700

    Fix unit tests

--- src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ODataPrimitiveDeserializer.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ODataPrimitiveDeserializer.cs
@@ -67,7 +67,7 @@ namespace Microsoft.AspNet.OData.Formatter.Deserialization
                 throw Error.ArgumentNull("primitiveProperty");
             }
             //Try and change the value appropriately if type is specified
-            if ( readContext!=null && readContext.ResourceType != null)
+            if ( readContext!=null && readContext.ResourceType != null && primitiveProperty.Value!=null)
             {
                 return EdmPrimitiveHelpers.ConvertPrimitiveValue(primitiveProperty.Value, readContext.ResourceType);
             }

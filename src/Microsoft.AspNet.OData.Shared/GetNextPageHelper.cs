0e75883b4664b26b43a148567bd074fb9b503a44
KanishManuja-MS <41647051+KanishManuja-MS@users.noreply.github.com>
2018-11-21 06:19:49 -0800

    Update GetNextPageHelper.cs

--- src/Microsoft.AspNet.OData.Shared/GetNextPageHelper.cs
+++ src/Microsoft.AspNet.OData.Shared/GetNextPageHelper.cs
@@ -66,7 +66,7 @@ namespace Microsoft.AspNet.OData
                         break;
                 }
 
-                if ((key=="$skip" && useSkipToken) || (key == "$skiptoken" && useSkipToken))
+                if ((key == "$skip" && useSkipToken) || (key == "$skiptoken" && useSkipToken))
                 {
                     continue;
                 }

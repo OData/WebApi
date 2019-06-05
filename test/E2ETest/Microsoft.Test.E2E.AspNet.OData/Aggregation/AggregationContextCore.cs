8964989e664797bce39cc4893918fc2843ef4ac5
Konstantin Kosinsky <kokosins@microsoft.com>
2019-06-05 11:10:05 -0700

    Ensure that we checking SQL for E2E aggregation tests

--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationContextCore.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationContextCore.cs
@@ -3,11 +3,19 @@
 
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
 {
     public class AggregationContextCoreBase : DbContext
     {
+        public static readonly LoggerFactory ConsoleLoggerFactory = new LoggerFactory
+            (new[] { new TraceLoggerProvider() });
+
+        public TraceLoggerProvider Provider { get; set; } = new TraceLoggerProvider();
+
+
         public DbSet<Customer> Customers { get; set; }
     }
 
@@ -27,7 +35,67 @@ namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=True;Persist Security Info = True;Database = AggregationEFCoreTest");
+            optionsBuilder.UseLoggerFactory(ConsoleLoggerFactory);
             base.OnConfiguring(optionsBuilder);
         }
     }
+
+    #region "SQL Logging"
+    public class TraceLogger : ILogger
+    {
+        private readonly string categoryName;
+
+        private Action<string> _callback;
+
+        public TraceLogger(Action<string> callback)
+        {
+            this.categoryName = DbLoggerCategory.Database.Command.Name;
+            this._callback = callback;
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(
+            LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception exception,
+            Func<TState, Exception, string> formatter)
+        {
+            this._callback?.Invoke(formatter(state, exception));
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+    }
+
+    public class TraceLoggerProvider : ILoggerProvider
+    {
+        public Microsoft.Extensions.Logging.ILogger CreateLogger(string categoryName)
+        {
+            return new TraceLogger(Add);
+        }
+
+        public static string CurrentSQL { get; set; }
+        public void Add(string txt)
+        {
+            if (txt.StartsWith("Executing DbCommand"))
+            {
+                CurrentSQL = txt;
+            }
+
+        }
+
+        public void Dispose() { }
+
+        internal static void CleanCommands()
+        {
+            CurrentSQL = "";
+        }
+    }
+    #endregion
+
+
 }
--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationEdmModel.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Aggregation/AggregationEdmModel.cs
@@ -22,6 +22,11 @@ namespace Microsoft.Test.E2E.AspNet.OData.Aggregation
             var builder = configuration.CreateConventionModelBuilder();
             builder.EntitySet<Customer>("Customers");
             builder.EntitySet<Order>("Orders");
+
+            var func = builder.Function("GetLastCommand");
+            func.Returns<string>();
+
+            var clean = builder.Function("CleanCommands").Returns<bool>();
             IEdmModel model = builder.GetEdmModel();
 
             var stdDevMethods = GetCustomMethods(typeof(DbFunctions), StdDevMethodLabel);

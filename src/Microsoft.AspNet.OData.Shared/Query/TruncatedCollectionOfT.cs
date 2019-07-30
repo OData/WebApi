9d084cd12472b4808747c0e0bbe3efd3e96e0428
adzhiljano <adzhiljano@users.noreply.github.com>
2019-07-30 06:26:38 +0300

    Fix EnableQuery attribute to take into account constant parameterization when PageSize is set (#1523) (#1535)
    * Fix EnableQuery to take into account constant parameterization when PageSize is set (#1523)
    
    When PageSize is being handled in the EnableQuery attribute it should respect the ODataQuerySettings.EnableConstantParameterization property. Otherwise, there is no way to force a generation of a SQL parameter in the resulting query. Which can result in polluting the SQL Server query plan cache with unwanted query plans (containing constants).
    
    * Fix build and resolve conflicts

--- src/Microsoft.AspNet.OData.Shared/Query/TruncatedCollectionOfT.cs
+++ src/Microsoft.AspNet.OData.Shared/Query/TruncatedCollectionOfT.cs
@@ -37,8 +37,20 @@ namespace Microsoft.AspNet.OData.Query
         /// <param name="pageSize">The page size.</param>
         // NOTE: The queryable version calls Queryable.Take which actually gets translated to the backend query where as 
         // the enumerable version just enumerates and is inefficient.
-        public TruncatedCollection(IQueryable<T> source, int pageSize)
-            : base(source.Take(checked(pageSize + 1)))
+        public TruncatedCollection(IQueryable<T> source, int pageSize) : this(source, pageSize, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TruncatedCollection{T}"/> class.
+        /// </summary>
+        /// <param name="source">The queryable collection to be truncated.</param>
+        /// <param name="pageSize">The page size.</param>
+        /// <param name="parameterize">Flag indicating whether constants should be parameterized</param>
+        // NOTE: The queryable version calls Queryable.Take which actually gets translated to the backend query where as 
+        // the enumerable version just enumerates and is inefficient.
+        public TruncatedCollection(IQueryable<T> source, int pageSize, bool parameterize)
+            : base(Take(source, pageSize, parameterize))
         {
             Initialize(pageSize);
         }
@@ -68,8 +80,21 @@ namespace Microsoft.AspNet.OData.Query
         /// <param name="totalCount">The total count.</param>
         // NOTE: The queryable version calls Queryable.Take which actually gets translated to the backend query where as 
         // the enumerable version just enumerates and is inefficient.
-        public TruncatedCollection(IQueryable<T> source, int pageSize, long? totalCount)
-            : base(pageSize > 0 ? source.Take(checked(pageSize + 1)) : source)
+        public TruncatedCollection(IQueryable<T> source, int pageSize, long? totalCount) : this(source, pageSize, totalCount, false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TruncatedCollection{T}"/> class.
+        /// </summary>
+        /// <param name="source">The queryable collection to be truncated.</param>
+        /// <param name="pageSize">The page size.</param>
+        /// <param name="totalCount">The total count.</param>
+        /// <param name="parameterize">Flag indicating whether constants should be parameterized</param>
+        // NOTE: The queryable version calls Queryable.Take which actually gets translated to the backend query where as 
+        // the enumerable version just enumerates and is inefficient.
+        public TruncatedCollection(IQueryable<T> source, int pageSize, long? totalCount, bool parameterize)
+            : base(pageSize > 0 ? Take(source, pageSize, parameterize) : source)
         {
             if (pageSize > 0)
             {
@@ -93,6 +118,16 @@ namespace Microsoft.AspNet.OData.Query
                 _isTruncated = true;
                 RemoveAt(Count - 1);
             }
+        }
+
+        private static IQueryable<T> Take(IQueryable<T> source, int pageSize, bool parameterize)
+        {
+            if (source == null)
+            {
+                throw Error.ArgumentNull("source");
+            }
+
+            return ExpressionHelpers.Take(source, checked(pageSize + 1), typeof(T), parameterize) as IQueryable<T>;
         }
 
         /// <inheritdoc />

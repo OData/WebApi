9abaec9024b9f49c57b245e37e1dab0caaa03575
Sam Xu <saxu@microsoft.com>
2019-12-19 13:59:33 -0800

    Update Nuget.exe and build.ps1

--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/QueryComposition/SecurityTests.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/QueryComposition/SecurityTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
+#if !NETCOREAPP3_0
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNet.OData.Extensions;
@@ -124,3 +125,4 @@ namespace Microsoft.Test.E2E.AspNet.OData.QueryComposition
         }
     }
 }
+#endif

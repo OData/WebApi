7f1c284057d9831dea4f9701febea7a88fbab11a
Lucas Loegel <lucas.loegel@enablon.com>
2020-03-02 11:20:02 -0800

    Fix #1953: Generation of Skiptoken value with DateTime

--- src/Microsoft.AspNet.OData.Shared/TimeZoneInfoHelper.cs
+++ src/Microsoft.AspNet.OData.Shared/TimeZoneInfoHelper.cs
@@ -22,5 +22,54 @@ namespace Microsoft.AspNet.OData
             }
             set { _defaultTimeZoneInfo = value; }
         }
+
+        public static DateTimeOffset ConvertToDateTimeOffset(DateTime dateTime)
+        {
+            TimeZoneInfo timeZone = TimeZoneInfoHelper.TimeZone;
+            TimeSpan utcOffset = timeZone.GetUtcOffset(dateTime);
+            if (utcOffset >= TimeSpan.Zero)
+            {
+                if (dateTime <= DateTime.MinValue + utcOffset)
+                {
+                    return DateTimeOffset.MinValue;
+                }
+            }
+            else
+            {
+                if (dateTime >= DateTime.MaxValue + utcOffset)
+                {
+                    return DateTimeOffset.MaxValue;
+                }
+            }
+
+            if (dateTime.Kind == DateTimeKind.Local)
+            {
+                TimeZoneInfo localTimeZoneInfo = TimeZoneInfo.Local;
+                TimeSpan localTimeSpan = localTimeZoneInfo.GetUtcOffset(dateTime);
+                if (localTimeSpan < TimeSpan.Zero)
+                {
+                    if (dateTime >= DateTime.MaxValue + localTimeSpan)
+                    {
+                        return DateTimeOffset.MaxValue;
+                    }
+                }
+                else
+                {
+                    if (dateTime <= DateTime.MinValue + localTimeSpan)
+                    {
+                        return DateTimeOffset.MinValue;
+                    }
+                }
+
+                return TimeZoneInfo.ConvertTime(new DateTimeOffset(dateTime), timeZone);
+            }
+
+            if (dateTime.Kind == DateTimeKind.Utc)
+            {
+                return TimeZoneInfo.ConvertTime(new DateTimeOffset(dateTime), timeZone);
+            }
+
+            return new DateTimeOffset(dateTime, timeZone.GetUtcOffset(dateTime));
+        }
     }
 }

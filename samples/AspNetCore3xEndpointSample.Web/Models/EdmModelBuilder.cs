d9b2f89721a13ebf8099370a15bde709dc162099
Sam Xu <saxu@microsoft.com>
2020-03-10 20:43:55 -0700

    Verify issue #2081: Auto expand on collection of property in derived class

--- samples/AspNetCore3xEndpointSample.Web/Models/EdmModelBuilder.cs
+++ samples/AspNetCore3xEndpointSample.Web/Models/EdmModelBuilder.cs
@@ -3,6 +3,8 @@
 
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.OData.Edm;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace AspNetCore3xEndpointSample.Web.Models
 {
@@ -17,11 +19,39 @@ namespace AspNetCore3xEndpointSample.Web.Models
                 var builder = new ODataConventionModelBuilder();
                 builder.EntitySet<Customer>("Customers");
                 builder.EntitySet<Order>("Orders");
+
+                builder.EntitySet<ImageAd>("ImageAds");
+                builder.EntitySet<Image>("Images");
                 _edmModel = builder.GetEdmModel();
             }
 
             return _edmModel;
         }
 
+    }
+
+    public class ImageAd
+    {
+        public int Id { get; set; }
+
+        [AutoExpand]
+        public IList<Image> Images { get; set; }
+
+        //public IEnumerable<Image> Images { get; set; }
+    }
+
+    public class SubImageAd : ImageAd
+    {
+        [AutoExpand]
+        public IList<Image> SubImages { get; set; }
+
+        //public IEnumerable<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
     }
 }

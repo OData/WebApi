c977556d5a2522713adf140a09faa2e1b8a1ce0c
Sreejith Pazhampilly <sreejithpin@gmail.com>
2020-08-14 15:20:46 -0700

    BulkInsert1 draft

---
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Build.AspNet/BulkInsert1/IBulkOperation.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.OData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Test.E2E.AspNet.OData.BulkInsert1
+{
+    public interface IBulkOperation
+    {
+        IEdmStructuredObject ApplyEntityOperation<T>(IEdmStructuredObject edmEntityObject, T objectType);
+    }
+}

ebe2a09eafe06711fd903c6bea341bb51be84c82
Kennedy Kangethe Munga <kennedy.munga@microsoft.com>
2020-09-16 22:24:12 -0700

    Fix #2262: Invalid partial JSON response happens expanding an entity with more items than the allowed PageSize, PR #2287
    Co-authored-by: mikepizzo <mikep@microsoft.com>

--- src/Microsoft.AspNet.OData.Shared/Formatter/Serialization/ODataResourceSetSerializer.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/Serialization/ODataResourceSetSerializer.cs
@@ -357,11 +357,29 @@ namespace Microsoft.AspNet.OData.Formatter.Serialization
         private static Uri GetNestedNextPageLink(ODataSerializerContext writeContext, int pageSize, object obj)
         {
             Contract.Assert(writeContext.ExpandedResource != null);
+            Uri navigationLink = null;
             IEdmNavigationSource sourceNavigationSource = writeContext.ExpandedResource.NavigationSource;
             NavigationSourceLinkBuilderAnnotation linkBuilder = writeContext.Model.GetNavigationSourceLinkBuilder(sourceNavigationSource);
-            Uri navigationLink =
-                linkBuilder.BuildNavigationLink(writeContext.ExpandedResource, writeContext.NavigationProperty);
+
+            // In Contained Navigation, we don't have navigation property binding,
+            // Hence we cannot get the NavigationLink from the NavigationLinkBuilder
+            if (writeContext.NavigationSource.NavigationSourceKind() == EdmNavigationSourceKind.ContainedEntitySet)
+            {
+                // Contained navigation.
+                Uri idlink = linkBuilder.BuildIdLink(writeContext.ExpandedResource);
+
+                var link = idlink.ToString() + "/" + writeContext.NavigationProperty.Name;
+                navigationLink = new Uri(link);
+            }
+            else
+            {
+                // Non-Contained navigation.
+                navigationLink =
+                    linkBuilder.BuildNavigationLink(writeContext.ExpandedResource, writeContext.NavigationProperty);
+            }
+
             Uri nestedNextLink = GenerateQueryFromExpandedItem(writeContext, navigationLink);
+
             SkipTokenHandler nextLinkGenerator = null;
             if (writeContext.QueryContext != null)
             {

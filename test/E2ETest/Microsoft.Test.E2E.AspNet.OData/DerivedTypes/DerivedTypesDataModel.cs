382c63fd057d6665fbd5b4f595abd31a1e3c9733
John Gathogo <john.gathogo@gmail.com>
2020-10-06 08:37:19 +0300

    Fix buggy behaviour when addressing derived types (#2310)
    * Fix buggy behaviour when addressing derived types
    
    * Make improvements suggested in review
    
    Co-authored-by: John Gathogo <jogathogo@microsoft.com>

---
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/DerivedTypes/DerivedTypesDataModel.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Test.E2E.AspNet.OData.DerivedTypes
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+    public class VipCustomer : Customer
+    {
+        public string LoyaltyCardNo { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/DerivedTypes/DerivedTypesTests.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNet.OData.Routing.Conventions;
+using Microsoft.OData.Edm;
+using Microsoft.Test.E2E.AspNet.OData.Common.Execution;
+using Xunit;
+
+namespace Microsoft.Test.E2E.AspNet.OData.DerivedTypes
+{
+    public class DerivedTypeTests : WebHostTestBase
+    {
+        public DerivedTypeTests(WebHostTestFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        protected override void UpdateConfiguration(WebRouteConfiguration configuration)
+        {
+            var controllers = new[] { typeof(CustomersController), typeof(MetadataController) };
+            configuration.AddControllers(controllers);
+
+            configuration.Routes.Clear();
+            configuration.Count().Filter().OrderBy().Expand().MaxTop(null).Select();
+            configuration.MapODataServiceRoute("odata", "odata", GetEdmModel(configuration), new DefaultODataPathHandler(), ODataRoutingConventions.CreateDefault());
+        }
+
+        private static IEdmModel GetEdmModel(WebRouteConfiguration configuration)
+        {
+            ODataConventionModelBuilder builder = configuration.CreateConventionModelBuilder();
+
+            builder.EntitySet<Customer>("Customers");
+            builder.EntityType<Order>();
+            builder.EntityType<VipCustomer>().DerivesFrom<Customer>();
+
+            return builder.GetEdmModel();
+        }
+
+        [Fact]
+        public async Task RestrictEntitySetToDerivedTypeInstances()
+        {
+            string requestUri = this.BaseAddress + "/odata/Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await this.Client.SendAsync(request);
+            Assert.True(response.IsSuccessStatusCode);
+
+            var expectedContent = string.Format("{{\"@odata.context\":\"{0}/odata/$metadata" +
+                "#Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer\"," +
+                "\"value\":[{{\"Id\":2,\"Name\":\"Customer 2\",\"LoyaltyCardNo\":\"9876543210\"}}]}}", this.BaseAddress);
+
+            Assert.Equal(expectedContent, await response.Content.ReadAsStringAsync());
+        }
+
+        [Theory]
+        [InlineData("Customers(2)/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer")]
+        [InlineData("Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer(2)")]
+        public async Task RestrictEntityToDerivedTypeInstance(string path)
+        {
+            // Key preceeds name of the derived type
+            string requestUri = this.BaseAddress + "/odata/" + path;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await this.Client.SendAsync(request);
+            Assert.True(response.IsSuccessStatusCode);
+
+            var expectedContent = string.Format("{{\"@odata.context\":\"{0}/odata/$metadata" +
+                "#Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer/$entity\"," +
+                "\"Id\":2,\"Name\":\"Customer 2\",\"LoyaltyCardNo\":\"9876543210\"}}", this.BaseAddress);
+
+            Assert.Equal(expectedContent, await response.Content.ReadAsStringAsync());
+        }
+
+        [Fact]
+        public async Task ReturnNotFound_ForKeyNotAssociatedWithDerivedType()
+        {
+            // Customer with Id 1 is not a VipCustomer
+            string requestUri = this.BaseAddress + "/odata/Customers(1)/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await this.Client.SendAsync(request);
+            Assert.False(response.IsSuccessStatusCode);
+            Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RestrictEntitySetToDerivedTypeInstances_ThenExpandNavProperty()
+        {
+            string requestUri = this.BaseAddress + "/odata/Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer?$expand=Orders";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await this.Client.SendAsync(request);
+            Assert.True(response.IsSuccessStatusCode);
+
+            var expectedContent = string.Format("{{\"@odata.context\":\"{0}/odata/$metadata" +
+                "#Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer(Orders())\"," +
+                "\"value\":[{{\"Id\":2,\"Name\":\"Customer 2\",\"LoyaltyCardNo\":\"9876543210\"," +
+                "\"Orders\":[{{\"Id\":2,\"Amount\":230}},{{\"Id\":3,\"Amount\":150}}]}}]}}", this.BaseAddress);
+
+            Assert.Equal(expectedContent, await response.Content.ReadAsStringAsync());
+        }
+
+        [Theory]
+        [InlineData("Customers(2)/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer?$expand=Orders")]
+        [InlineData("Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer(2)?$expand=Orders")]
+        public async Task RestrictEntityToDerivedTypeInstance_ThenExpandNavProperty(string pathAndQuery)
+        {
+            // Key preceeds name of the derived type
+            string requestUri = this.BaseAddress + "/odata/" + pathAndQuery;
+
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await this.Client.SendAsync(request);
+            Assert.True(response.IsSuccessStatusCode);
+
+            var expectedContent = string.Format("{{\"@odata.context\":\"{0}/odata/$metadata" +
+                "#Customers/Microsoft.Test.E2E.AspNet.OData.DerivedTypes.VipCustomer(Orders())/$entity\"," +
+                "\"Id\":2,\"Name\":\"Customer 2\",\"LoyaltyCardNo\":\"9876543210\"," +
+                "\"Orders\":[{{\"Id\":2,\"Amount\":230}},{{\"Id\":3,\"Amount\":150}}]}}", this.BaseAddress);
+
+            Assert.Equal(expectedContent, await response.Content.ReadAsStringAsync());
+        }
+    }
+}

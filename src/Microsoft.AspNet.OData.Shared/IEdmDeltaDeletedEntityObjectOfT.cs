46b09a6364d08d632bbd4298dcb9d8108b41d536
Sreejith Pazhampilly <sreejithpin@gmail.com>
2020-11-13 10:21:41 -0800

    updates

--- src/Microsoft.AspNet.OData.Shared/IEdmDeltaDeletedEntityObjectOfT.cs
+++ src/Microsoft.AspNet.OData.Shared/IEdmDeltaDeletedEntityObjectOfT.cs
@@ -11,14 +11,6 @@ namespace Microsoft.AspNet.OData
     /// </summary>
     public interface IEdmDeltaDeletedEntityObject<TStructuralType> : IEdmChangedObject<TStructuralType>, IEdmDeltaDeletedEntityObject
     {
-        /// <summary>
-        /// The id of the deleted entity (same as the odata.id returned or computed when calling GET on resource), which may be absolute or relative.
-        /// </summary>
-        string Id { get; set; }
 
-        /// <summary>
-        /// Optional. Either deleted, if the entity was deleted (destroyed), or changed if the entity was removed from membership in the result (i.e., due to a data change).
-        /// </summary>
-        DeltaDeletedEntryReason Reason { get; set; }
     }
 }

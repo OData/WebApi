0198ea4c197371cadc41d17379a66e795a15d097
Kenneth Carter <kccarter32@gmail.com>
2021-01-14 21:58:15 -0700

    seperated the interface and class name space for this sut into seperate files.

--- test/UnitTest/Microsoft.AspNet.OData.Test.Shared/Formatter/SUT/CollectionSerializerCustomersControllerClass.cs
+++ test/UnitTest/Microsoft.AspNet.OData.Test.Shared/Formatter/SUT/CollectionSerializerCustomersControllerClass.cs
@@ -22,22 +22,3 @@ namespace Microsoft.AspNet.OData.Test.Formatter.Class
     }
 }
 
-namespace Microsoft.AspNet.OData.Test.Formatter.Interface
-{
-    public class CollectionSerializerCustomersController : TestODataController
-    {
-        public ITestActionResult Get(IODataQueryOptions<CollectionSerializerCustomer> options)
-        {
-            IQueryable<CollectionSerializerCustomer> customers = new[]
-            {
-                    new CollectionSerializerCustomer{ID = 1, Name = "Name 1"},
-                    new CollectionSerializerCustomer{ID = 2, Name = "Name 2"},
-                    new CollectionSerializerCustomer{ID = 3, Name = "Name 3"},
-                }.AsQueryable();
-
-            IQueryable<IEdmEntityObject> appliedCustomers = options.ApplyTo(customers) as IQueryable<IEdmEntityObject>;
-
-            return Ok(appliedCustomers);
-        }
-    }
-}

20ce568b22ffab87b45f0b872a77760aa2db73b4
Sreejith Pazhampilly <37782322+Sreejithpin@users.noreply.github.com>
2021-06-03 06:10:47 -0700

    Bulk operations8 (#2502)
    * Bulk operations6 (#2)
    
    * BulkInsert1 draft
    
    * Bulk insert changes
    
    * deleted old test
    
    * updates
    
    * Update ODataResourceSetWrapper.cs
    
    * updates
    
    * Updated addressing  comments
    
    * updates
    
    * updates
    
    updates
    
    * Updates
    
    * Updates
    
    * Minor updates
    
    * comments
    
    * updates
    
    * update publicapi for core
    
    * Address comments
    
    * Cleanup and additional tests
    
    Cleanup and additional tests
    
    * Updated code
    
    * BulkInsert1 draft
    
    * Bulk insert changes
    
    * deleted old test
    
    * updates
    
    * updates
    
    * Updated addressing  comments
    
    * updates
    
    * updates
    
    updates
    
    * Updates
    
    * Updates
    
    * Minor updates
    
    * updates
    
    * Address comments
    
    * Cleanup and additional tests
    
    Cleanup and additional tests
    
    * DataAnnotationException updates
    
    * comments
    
    * small updates
    
    * updates
    
    * small update
    
    * updates
    
    * Updates
    
    * Update DeltaSetOfT.cs
    
    * Updates with Patch
    
    * updates
    
    * updates
    
    * Update WebHostTestFixture.cs
    
    * updates
    
    * Update DeltaOfTStructuralType.cs
    
    * Updates
    
    * Updates for serializer etc
    
    * Update WebHostTestFixture.cs
    
    * updates
    
    * updates
    
    * updates
    
    * Bulk Operations Updates
    
    * Review comments addressed
    
    * Updates
    
    * updated for deltaentity
    
    * Update Microsoft.AspNet.OData.Test.csproj
    
    * Update Microsoft.AspNet.OData.PublicApi.bsl
    
    * update public api
    
    * test fix
    
    * Update EdmStructuredObject.cs
    
    * Update EdmStructuredObject.cs
    
    * Update BulkInsertTest.cs
    
    * update
    
    * public api
    
    * review comments
    
    * Review comments updates
    
    * smalll update
    
    * updates
    
    * updates
    
    * updates

---
+++ src/Microsoft.AspNet.OData.Shared/DefaultPatchHandler.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.OData
+{    
+    internal class DefaultPatchHandler<TStructuralType> : PatchMethodHandler<TStructuralType> where TStructuralType :class
+    {
+        Type _clrType;
+        ICollection<TStructuralType> originalList;
+
+        public DefaultPatchHandler(ICollection<TStructuralType> originalList)
+        {
+            this._clrType = typeof(TStructuralType);
+            this.originalList = originalList?? new List<TStructuralType>();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out TStructuralType originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = default(TStructuralType);
+
+            try
+            {
+                originalObject = GetFilteredItem(keyValues);
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryCreate(Delta<TStructuralType> patchObject, out TStructuralType createdObject, out string errorMessage)
+        {
+            createdObject = default(TStructuralType);
+            errorMessage = string.Empty;
+
+            try
+            {
+                if(originalList != null)
+                {
+                    originalList = new List<TStructuralType>();
+                }
+
+                createdObject = Activator.CreateInstance(_clrType) as TStructuralType;
+                originalList.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                TStructuralType originalObject = GetFilteredItem(keyValues);
+                originalList.Remove(originalObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override IPatchMethodHandler GetNestedPatchHandler(TStructuralType parent, string navigationPropertyName)
+        {
+            foreach (PropertyInfo property in _clrType.GetProperties())
+            {
+                if (property.Name == navigationPropertyName)
+                {
+                    Type type = typeof(DefaultPatchHandler<>).MakeGenericType(property.PropertyType.GetGenericArguments()[0]);
+
+                    return Activator.CreateInstance(type, property.GetValue(parent)) as IPatchMethodHandler;
+                }
+            }
+
+            return null;
+        }
+
+
+        private TStructuralType GetFilteredItem(IDictionary<string, object> keyValues)
+        {
+            //This logic is for filtering the object based on the set of keys,
+            //There will only be very few key elements usually, mostly 1, so performance wont be impacted.
+
+            if(originalList == null || originalList.Count == 0)
+            {
+                return default(TStructuralType);
+            }
+
+            Dictionary<string, PropertyInfo> propertyInfos = new Dictionary<string, PropertyInfo>();
+
+            foreach (string key in keyValues.Keys)
+            {
+                propertyInfos.Add(key, _clrType.GetProperty(key));
+            }
+
+            foreach (TStructuralType item in originalList)
+            {
+                bool isMatch = true;
+
+                foreach (KeyValuePair<string, object> keyValue in keyValues)
+                {
+                    if (!Equals(propertyInfos[keyValue.Key].GetValue(item), keyValue.Value))
+                    {
+                        // Not a match, so try the next one
+                        isMatch = false;
+                        break;
+                    }
+                }
+
+                if (isMatch)
+                {
+                    return item;
+                }
+            }
+
+            return default(TStructuralType);
+        }  
+    }
+}

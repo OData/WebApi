d7a11874de6342d21d9337b80e2637ba645712ed
Sreejith Pazhampilly <37782322+Sreejithpin@users.noreply.github.com>
2021-06-03 06:10:47 -0700

    Bulk operations8 (#2502)

---
+++ src/Microsoft.AspNet.OData.Shared/DefaultEdmPatchMethodHandler.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.AspNet.OData
+{
+    internal class DefaultEdmPatchMethodHandler : EdmPatchMethodHandler
+    {
+        IEdmEntityType entityType;
+        ICollection<IEdmStructuredObject> originalList;
+
+        public DefaultEdmPatchMethodHandler(ICollection<IEdmStructuredObject> originalList, IEdmEntityType entityType)
+        {
+            Contract.Assert(entityType != null);
+
+            this.entityType = entityType;
+            this.originalList = originalList?? new List<IEdmStructuredObject>();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out IEdmStructuredObject originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = null;
+
+            Contract.Assert(keyValues != null);
+
+            try
+            {
+                originalObject = GetFilteredItem(keyValues);
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryCreate(IEdmChangedObject changedObject, out IEdmStructuredObject createdObject, out string errorMessage)
+        {
+            createdObject = null;
+            errorMessage = string.Empty;
+
+            try
+            {
+                createdObject = new EdmEntityObject(entityType);
+                originalList.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                EdmStructuredObject originalObject = GetFilteredItem(keyValues);
+                
+                if (originalObject != null)
+                {
+                    originalList.Remove(originalObject);
+                }
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override EdmPatchMethodHandler GetNestedPatchHandler(IEdmStructuredObject parent, string navigationPropertyName)
+        {
+            IEdmNavigationProperty navProperty = entityType.NavigationProperties().FirstOrDefault(navProp => navProp.Name == navigationPropertyName);
+
+            if(navProperty == null)
+            {
+                return null;
+            }
+
+            IEdmEntityType nestedEntityType = navProperty.ToEntityType();
+
+            object obj;
+            if(parent.TryGetPropertyValue(navigationPropertyName, out obj))
+            {
+                ICollection<IEdmStructuredObject> nestedList = obj as ICollection<IEdmStructuredObject>;
+
+                return new DefaultEdmPatchMethodHandler(nestedList, nestedEntityType);
+            }            
+
+            return null;
+        }
+
+
+        private EdmStructuredObject GetFilteredItem(IDictionary<string, object> keyValues)
+        {
+            //This logic is for filtering the object based on the set of keys,
+            //There will only be very few key elements usually, mostly 1, so performance wont be impacted.
+
+            if(originalList == null)
+            {
+                return null;
+            }
+
+            foreach (EdmStructuredObject item in originalList)
+            {
+                bool isMatch = true;
+
+                foreach (KeyValuePair<string, object> keyValue in keyValues)
+                {
+                    object value;
+                    if (item.TryGetPropertyValue(keyValue.Key, out value))
+                    {
+                        if (!Equals(value, keyValue.Value))
+                        {
+                            // Not a match, so try the next one
+                            isMatch = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isMatch)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Build.AspNetCore3x/BulkOperation/EFTests/BulkOperationPatchHandlersEF.cs
@@ -0,0 +1,397 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.OData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Test.E2E.AspNet.OData.BulkInsert;
+
+namespace Microsoft.Test.E2E.AspNet.OData.BulkOperation
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Friend> Friends { get; set; }
+
+        protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(c => c.ID);
+            modelBuilder.Entity<Employee>().Ignore(c => c.SkillSet);
+            modelBuilder.Entity<Employee>().Ignore(c => c.NewFriends);
+            modelBuilder.Entity<Employee>().Ignore(c => c.UnTypedFriends);
+            modelBuilder.Entity<Employee>().Ignore(c => c.InstanceAnnotations);
+
+            modelBuilder.Entity<NewFriend>().Ignore(c => c.InstanceAnnotations);
+            modelBuilder.Entity<UnTypedFriend>().Ignore(c => c.InstanceAnnotations);
+
+            modelBuilder.Entity<Friend>().HasKey(c => c.Id);
+            
+        }
+
+       
+    }
+
+    public class EmployeeEFPatchHandler : PatchMethodHandler<Employee>
+    {
+        EmployeeDBContext dbContext = null;
+
+        public EmployeeEFPatchHandler(EmployeeDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public override PatchStatus TryCreate(Delta<Employee> deltaObject, out Employee createdObject, out string errorMessage)
+        {
+            createdObject = null;
+            errorMessage = string.Empty;
+
+            try
+            {
+                createdObject = new Employee();
+                dbContext.Employees.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                var id = keyValues.First().Value.ToString();
+                var customer = dbContext.Employees.First(x => x.ID == Int32.Parse(id));
+
+                dbContext.Employees.Remove(customer);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out Employee originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = null;
+
+            try
+            {
+                var id = keyValues["ID"].ToString();
+                originalObject = dbContext.Employees.First(x => x.ID == Int32.Parse(id));
+
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        public override IPatchMethodHandler GetNestedPatchHandler(Employee parent, string navigationPropertyName)
+        {
+            switch (navigationPropertyName)
+            {
+                case "Friends":
+                    return new FriendEFPatchHandler(parent);
+                case "NewFriends":
+                    return new NewFriendEFPatchHandler(parent);
+                default:
+                    return null;
+            }
+            
+        }
+
+    }
+
+    public class FriendEFPatchHandler : PatchMethodHandler<Friend>
+    {
+        Employee employee;
+        public FriendEFPatchHandler(Employee employee)
+        {
+            this.employee = employee;
+        }
+
+        public override PatchStatus TryCreate(Delta<Friend> deltaObject, out Friend createdObject, out string errorMessage)
+        {
+            createdObject = null;
+            errorMessage = string.Empty;
+
+            try
+            {
+                createdObject = new Friend();
+                employee.Friends.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                var id = keyValues.First().Value.ToString();
+                var friend = employee.Friends.First(x => x.Id == Int32.Parse(id));
+
+                employee.Friends.Remove(friend);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out Friend originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = null;
+
+            try
+            {
+                var id = keyValues["Id"].ToString();
+                if (employee.Friends == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+                else
+                {
+                    originalObject = employee.Friends.FirstOrDefault(x => x.Id == Int32.Parse(id));
+                }
+
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        public override IPatchMethodHandler GetNestedPatchHandler(Friend parent, string navigationPropertyName)
+        {
+            return new OrderEFPatchHandler(parent);
+        }
+
+    }
+
+
+    public class NewFriendEFPatchHandler : PatchMethodHandler<NewFriend>
+    {
+        Employee employee;
+        public NewFriendEFPatchHandler(Employee employee)
+        {
+            this.employee = employee;
+        }
+
+        public override PatchStatus TryCreate(Delta<NewFriend> deltaObject, out NewFriend createdObject, out string errorMessage)
+        {
+            createdObject = null;
+            errorMessage = string.Empty;
+
+            try
+            {
+                createdObject = new NewFriend();
+                employee.NewFriends.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                var id = keyValues.First().Value.ToString();
+                var friend = employee.NewFriends.First(x => x.Id == Int32.Parse(id));
+
+                employee.NewFriends.Remove(friend);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out NewFriend originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = null;
+
+            try
+            {
+                var id = keyValues["Id"].ToString();
+                originalObject = employee.NewFriends.First(x => x.Id == Int32.Parse(id));
+
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        public override IPatchMethodHandler GetNestedPatchHandler(NewFriend parent, string navigationPropertyName)
+        {
+            return null;
+        }
+
+    }
+
+
+
+    public class OrderEFPatchHandler : PatchMethodHandler<Order>
+    {
+        Friend friend;
+        public OrderEFPatchHandler(Friend friend)
+        {
+            this.friend = friend;
+        }
+
+        public override PatchStatus TryCreate(Delta<Order> deltaObject, out Order createdObject, out string errorMessage)
+        {
+            createdObject = null;
+            errorMessage = string.Empty;
+
+            try
+            {
+                createdObject = new Order();
+                friend.Orders.Add(createdObject);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            try
+            {
+                var id = keyValues.First().Value.ToString();
+                var order = friend.Orders.First(x => x.Id == Int32.Parse(id));
+
+                friend.Orders.Remove(order);
+
+                return PatchStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+
+                return PatchStatus.Failure;
+            }
+        }
+
+        public override PatchStatus TryGet(IDictionary<string, object> keyValues, out Order originalObject, out string errorMessage)
+        {
+            PatchStatus status = PatchStatus.Success;
+            errorMessage = string.Empty;
+            originalObject = null;
+
+            try
+            {
+                var id = keyValues["Id"].ToString();
+                originalObject = friend.Orders.First(x => x.Id == Int32.Parse(id));
+
+
+                if (originalObject == null)
+                {
+                    status = PatchStatus.NotFound;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                status = PatchStatus.Failure;
+                errorMessage = ex.Message;
+            }
+
+            return status;
+        }
+
+        public override IPatchMethodHandler GetNestedPatchHandler(Order parent, string navigationPropertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

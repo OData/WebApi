d6fa12e924617094ad84cc02ffdfe1d35d46f0a6
ysmoradi <ysmoradi@outlook.com>
2021-07-09 12:42:32 +0430

    Fixed Enum try parse issue. This closes #2518

--- src/Microsoft.AspNet.OData.Shared/Formatter/ODataModelBinderConverter.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/ODataModelBinderConverter.cs
@@ -31,7 +31,9 @@ namespace Microsoft.AspNet.OData.Formatter
     public static class ODataModelBinderConverter
     {
         private static readonly MethodInfo EnumTryParseMethod = typeof(Enum).GetMethods()
-            .Single(m => m.Name == "TryParse" && m.GetParameters().Length == 2);
+            .Single(m => m.Name.Equals("TryParse", StringComparison.Ordinal)
+                && m.GetParameters().Length == 2
+                && m.GetParameters()[0].ParameterType.Equals(typeof(string)));
 
         private static readonly MethodInfo CastMethodInfo = typeof(Enumerable).GetMethod("Cast");
 

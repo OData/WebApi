664c7ddc0948c491e5b49751078eed5b1b289c13
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-07-27 07:26:40 -0700

    Code review comments

--- src/Microsoft.AspNet.OData.Shared/IDeltaSet.cs
+++ src/Microsoft.AspNet.OData.Shared/IDeltaSet.cs
@@ -1,14 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Microsoft.AspNet.OData
 {
     /// <summary>
     /// Basic interface to reperesent a deltaset which is a collection of Deltas.
+    /// This is being implemented by Deltaset{TStructuralType}. Since its being implementd by a gemeric type and
+    /// since we need to check in a few places(like deserializer) where the object is a DeltaSet and the {TStructuralType} is not available,
+    /// we need a marker interface which can be used in these checks.
     /// </summary>
     public interface IDeltaSet
     {
--- test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Build.AspNetCore3x/BulkOperation/EFTests/BulkInsertTestEF.cs
+++ test/E2ETest/Microsoft.Test.E2E.AspNet.OData/Build.AspNetCore3x/BulkOperation/EFTests/BulkInsertTestEF.cs
@@ -105,7 +105,7 @@ namespace Microsoft.Test.E2E.AspNet.OData.BulkInsert
             
             string requestUri = this.BaseAddress + "/convention/Employees";
 
-            var content = @"{'@odata.context':'http://host/service/$metadata#Employees/$delta',     
+            var content = @"{'@odata.context':'http://localhost:11001/convention/$metadata#Employees/$delta',     
                     'value':[{ '@odata.type': '#Microsoft.Test.E2E.AspNet.OData.BulkInsert.Employee', 'ID':1,'Name':'Employee1',
                             'Friends@odata.delta':[{'Id':1,'Name':'Friend1',
                             'Orders@odata.delta' :[{'Id':1,'Price': 10}, {'Id':2,'Price': 20} ] },{'Id':2,'Name':'Friend2'}]

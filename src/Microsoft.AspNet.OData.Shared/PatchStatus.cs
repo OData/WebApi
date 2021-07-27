3be8a612b8d1f69e5cf275588ca2bf60fcabdfbb
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-07-27 07:26:40 -0700

    Code review comments

---
+++ src/Microsoft.AspNet.OData.Shared/PatchStatus.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+namespace Microsoft.AspNet.OData
+{
+    /// <summary>
+    /// Enum for Patch Status
+    /// </summary>
+    public enum PatchStatus
+    {
+        /// <summary>
+        /// Success Status
+        /// </summary>
+        Success,
+        /// <summary>
+        /// Failure Status
+        /// </summary>
+        Failure,
+        /// <summary>
+        /// Resource Not Found
+        /// </summary>
+        NotFound
+    }
+}

0638561fa3a974a6f097caeb563c72879627f5d3
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-08-24 13:00:27 -0700

    Bulk Ops Handlers

---
+++ src/Microsoft.AspNet.OData.Shared/EdmODataAPIHandler.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------------
+// <copyright file="EdmODataAPIHandler.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved. 
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.OData
+{
+
+    /// <summary>
+    /// Handler Class to handle users methods for create, delete and update.
+    /// This is the handler for data modification where there is no CLR type.
+    /// </summary>
+    public abstract class EdmODataAPIHandler
+    {
+        /// <summary>
+        /// TryCreate method to create a new object.
+        /// </summary>
+        /// <param name="changedObject">Changed object which can be appied on creted object, optional</param>
+        /// <param name="createdObject">The created object (Typeless)</param>
+        /// <param name="errorMessage">Any error message in case of an exception</param>
+        /// <returns>The status of the TryCreate Method, statuses are <see cref="ODataAPIResponseStatus"/></returns>
+        public abstract ODataAPIResponseStatus TryCreate(IEdmChangedObject changedObject, out IEdmStructuredObject createdObject, out string errorMessage);
+
+        /// <summary>
+        ///  TryGet method which tries to get the Origignal object based on a keyvalues.
+        /// </summary>
+        /// <param name="keyValues">Key value pair for the entity keys</param>        
+        /// <param name="originalObject">Object to return</param>
+        /// <param name="errorMessage">Any error message in case of an exception</param>
+        /// <returns>The status of the TryGet Method, statuses are <see cref="ODataAPIResponseStatus"/></returns>
+        public abstract ODataAPIResponseStatus TryGet(IDictionary<string, object> keyValues, out IEdmStructuredObject originalObject, out string errorMessage);
+
+        /// <summary>
+        ///  TryDelete Method which will delete the object based on keyvalue pairs.
+        /// </summary>
+        /// <param name="keyValues"></param>
+        /// <param name="errorMessage"></param>
+        /// <returns>The status of the TryDelete Method, statuses are <see cref="ODataAPIResponseStatus"/></returns>
+        public abstract ODataAPIResponseStatus TryDelete(IDictionary<string, object> keyValues, out string errorMessage);
+
+        /// <summary>
+        /// Get the API handler for the nested type
+        /// </summary>
+        /// <param name="parent">Parent instance.</param>
+        /// <param name="navigationPropertyName">The name of the navigation property for the handler</param>
+        /// <returns>Nested Patch Method handler for the navigation property</returns>
+        public abstract EdmODataAPIHandler GetNestedHandler(IEdmStructuredObject parent, string navigationPropertyName);
+    }
+}
---
+++ test/UnitTest/Microsoft.AspNet.OData.Test.Shared/ODataPathExtensionsTest.cs
@@ -0,0 +1,208 @@
+//-----------------------------------------------------------------------------
+// <copyright file="ODataPathExtensionsTest.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved. 
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
+
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Test.Common.Models;
+using Microsoft.OData.UriParser;
+using Xunit;
+
+namespace Microsoft.AspNet.OData.Test
+{
+    public class ODataPathExtensionsTest
+    {
+        SampleEdmModel model = new SampleEdmModel();
+        KeyValuePair<string, object>[] customerKey = new[] { new KeyValuePair<string, object>("Id", "1") };
+        KeyValuePair<string, object>[] friendsKey = new[] { new KeyValuePair<string, object>("Id", "1001") };
+
+        [Fact]
+        public void GetKeys_PathWithTypeSegmentReturnsKeysFromLastKeySegment()
+        {
+            // From this path Customers(1)
+            // GetKeys() should return { "Id": "1" }
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet),
+                new KeySegment(customerKey, model.customerType, model.customerSet),
+                new TypeSegment(model.vipCustomerType, model.customerType, null)
+            });
+
+            // Act
+            Dictionary<string, object> keys = path.GetKeys();
+
+            // Assert
+            Assert.Single(keys);
+            Assert.Equal("Id", keys.First().Key);
+            Assert.Equal("1", keys.First().Value);
+        }
+
+        [Fact]
+        public void GetKeys_PathWithNoSegmentReturnsEmptyCollection()
+        {
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+            });
+
+            // Act
+            Dictionary<string, object> keys = path.GetKeys();
+
+            // Assert
+            Assert.Empty(keys);
+        }
+
+        [Fact]
+        public void GetKeys_PathWithNoKeySegmentReturnsEmptyCollection()
+        {
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet),
+                new TypeSegment(model.vipCustomerType, model.customerType, null)
+            });
+
+            // Act
+            Dictionary<string, object> keys = path.GetKeys();
+
+            // Assert
+            Assert.Empty(keys);
+        }
+
+        [Fact]
+        public void GetKeys_PathWithNavPropReturnsKeysFromLastKeySegment()
+        {
+            // From this path Customers(1)/Friends(1001)
+            // GetKeys() should return { "Id": "1001" }
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet),
+                new KeySegment(customerKey, model.customerType, model.customerSet),
+                new NavigationPropertySegment(model.friendsProperty, model.customerSet),
+                new KeySegment(friendsKey, model.personType, null)
+            });
+
+            // Act
+            Dictionary<string, object> keys = path.GetKeys();
+
+            // Assert
+            Assert.Single(keys);
+            Assert.Equal("Id", keys.First().Key);
+            Assert.Equal("1001", keys.First().Value);
+        }
+
+        [Fact]
+        public void GetLastNonTypeNonKeySegment_TypeSegmentAsLastSegmentReturnsCorrectSegment()
+        {
+            // If the path is Customers(1)/Friends(1001)/Ns.UniquePerson where Ns.UniquePerson is a type segment
+            // and 1001 is a KeySegment,
+            // GetLastNonTypeNonKeySegment() should return Friends NavigationPropertySegment.
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet),
+                new KeySegment(customerKey, model.customerType, model.customerSet),
+                new NavigationPropertySegment(model.friendsProperty, model.customerSet),
+                new KeySegment(friendsKey, model.personType, null),
+                new TypeSegment(model.uniquePersonType, model.personType, null)
+            });
+
+            // Act
+            ODataPathSegment segment = path.GetLastNonTypeNonKeySegment();
+
+            // Assert
+            Assert.Equal("Friends", segment.Identifier);
+            Assert.True(segment is NavigationPropertySegment);
+        }
+
+        [Fact]
+        public void GetLastNonTypeNonKeySegment_KeySegmentAsLastSegmentReturnsCorrectSegment()
+        {
+            // If the path is Customers(1)/Friends(1001) where1001 is a KeySegment,
+            // GetLastNonTypeNonKeySegment() should return Friends NavigationPropertySegment.
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet),
+                new KeySegment(customerKey, model.customerType, model.customerSet),
+                new NavigationPropertySegment(model.friendsProperty, model.customerSet),
+                new KeySegment(friendsKey, model.personType, null)
+            });
+
+            // Act
+            ODataPathSegment segment = path.GetLastNonTypeNonKeySegment();
+
+            // Assert
+            Assert.Equal("Friends", segment.Identifier);
+            Assert.True(segment is NavigationPropertySegment);
+        }
+
+        [Fact]
+        public void GetLastNonTypeNonKeySegment_SingleSegmentPathReturnsCorrectSegment()
+        {
+            // If the path is /Customers,
+            // GetLastNonTypeNonKeySegment() should return Customers EntitySetSegment.
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new EntitySetSegment(model.customerSet)
+            });
+
+            // Act
+            ODataPathSegment segment = path.GetLastNonTypeNonKeySegment();
+
+            // Assert
+            Assert.True(segment is EntitySetSegment);
+        }
+
+        [Fact]
+        public void GetLastNonTypeNonKeySegment_SingleKeySegmentPathReturnsNull()
+        {
+            // If the path is /1,
+            // GetLastNonTypeNonKeySegment() should return null since this is a KeySegment.
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new KeySegment(customerKey, model.customerType, model.customerSet)
+            });
+
+            // Act
+            ODataPathSegment segment = path.GetLastNonTypeNonKeySegment();
+
+            // Assert
+            Assert.Null(segment);
+        }
+
+        [Fact]
+        public void GetLastNonTypeNonKeySegment_SingleTypeSegmentPathReturnsNull()
+        {
+            // If the path is /Ns.UniquePerson,
+            // GetLastNonTypeNonKeySegment() should return null since this is a TypeSegment.
+
+            // Arrange
+            ODataPath path = new ODataPath(new ODataPathSegment[]
+            {
+                new TypeSegment(model.uniquePersonType, model.personType, null)
+            });
+
+            // Act
+            ODataPathSegment segment = path.GetLastNonTypeNonKeySegment();
+
+            // Assert
+            Assert.Null(segment);
+        }
+    }
+}

2f60d3890cc6e68eac338c42464385baf797496b
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-11-15 13:54:41 -0800

    Update for PAtch Complex type and return TStructuralType

--- src/Microsoft.AspNet.OData.Shared/DeltaOfTStructuralType.cs
+++ src/Microsoft.AspNet.OData.Shared/DeltaOfTStructuralType.cs
@@ -343,7 +343,7 @@ namespace Microsoft.AspNet.OData
         /// to the <paramref name="original"/> entity recursively.
         /// </summary>
         /// <param name="original">The entity to be updated.</param>
-        public void CopyChangedValues(TStructuralType original)
+        public TStructuralType CopyChangedValues(TStructuralType original)
         {
             if (original == null)
             {
@@ -403,11 +403,14 @@ namespace Microsoft.AspNet.OData
                     if (deltaNestedResource.IsComplexType && newType != originalType)
                     {
                         originalNestedResource = ReAssignComplexDerivedType(original, nestedResourceName, originalNestedResource, newType, originalType, deltaNestedResource.ExpectedClrType);
+                        _structuredType.GetProperty(nestedResourceName).SetValue(original, (object)originalNestedResource);
                     }
 
                     deltaNestedResource.CopyChangedValues(originalNestedResource);
                 }
             }
+
+            return original;
         }
 
         private dynamic ReAssignComplexDerivedType(TStructuralType parent, string nestedPropertyName, dynamic originalValue, Type newType, Type originalType, Type declaredType)
@@ -415,6 +418,11 @@ namespace Microsoft.AspNet.OData
             //As per OASIS discussion, changing a complex type from 1 derived type to another is allowed if both derived type have a common ancestor and the property
             //is declared in terms of a common ancestor. The logic below checks for a common ancestor. Create a new object of the derived type in delta request.
             //And copy the common properties.
+
+            if(newType == originalType)
+            {
+                return originalValue;
+            }
 
             Type newBaseType = newType;
             HashSet<Type> newBaseTypes = new HashSet<Type>();
@@ -451,8 +459,6 @@ namespace Microsoft.AspNet.OData
                         property.SetValue(newOriginalNestedResource, value);
                     }
 
-                    _structuredType.GetProperty(nestedPropertyName).SetValue(parent, (object)newOriginalNestedResource);
-
                     break;
                 }
 
@@ -488,14 +494,20 @@ namespace Microsoft.AspNet.OData
             CopyUnchangedDynamicValues(original);
         }
 
+
         /// <summary>
         /// Overwrites the <paramref name="original"/> entity with the changes tracked by this Delta.
         /// <remarks>The semantics of this operation are equivalent to a HTTP PATCH operation, hence the name.</remarks>
         /// </summary>
         /// <param name="original">The entity to be updated.</param>
-        public void Patch(TStructuralType original)
+        public TStructuralType Patch(TStructuralType original)
         {
-            CopyChangedValues(original);
+            if (IsComplexType)
+            {
+                original = ReAssignComplexDerivedType(null, "", original, _structuredType, original.GetType(), ExpectedClrType) as TStructuralType;                
+            }
+
+            return CopyChangedValues(original);           
         }
 
         /// <summary>

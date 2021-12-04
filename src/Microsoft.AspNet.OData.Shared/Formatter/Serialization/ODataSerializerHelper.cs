f5c1720ed2368e5904ee0a411c2aba4569f4a315
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-12-03 17:02:33 -0800

    updates

--- src/Microsoft.AspNet.OData.Shared/Formatter/Serialization/ODataSerializerHelper.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/Serialization/ODataSerializerHelper.cs
@@ -1,13 +1,15 @@
-﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
-// Licensed under the MIT License.  See License.txt in the project root for license information.
+﻿//-----------------------------------------------------------------------------
+// <copyright file="ODataSerializerHelper.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved. 
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
 
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.OData;
 using Microsoft.OData.Edm;
-using Org.OData.Core.V1;
 
 namespace Microsoft.AspNet.OData.Formatter.Serialization
 {

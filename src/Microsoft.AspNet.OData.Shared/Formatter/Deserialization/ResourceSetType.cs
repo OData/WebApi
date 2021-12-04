fac3b7bb3fdaa52f4f156ee38bbc07c108883e2a
Sreejith Pazhampilly <sreejithpin@gmail.com>
2021-12-03 17:02:33 -0800

    updates

--- src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ResourceSetType.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ResourceSetType.cs
@@ -1,9 +1,9 @@
-﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
-// Licensed under the MIT License.  See License.txt in the project root for license information.
-
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//-----------------------------------------------------------------------------
+// <copyright file="ResourceSetType.cs" company=".NET Foundation">
+//      Copyright (c) .NET Foundation and Contributors. All rights reserved. 
+//      See License.txt in the project root for license information.
+// </copyright>
+//------------------------------------------------------------------------------
 
 namespace Microsoft.AspNet.OData.Formatter.Deserialization
 {

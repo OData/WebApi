06543346d25018135d92aeb52c09958c789a4f65
Michael Pizzo <mikep@microsoft.com>
2022-01-11 14:51:42 -0800

    Draft PR to experiment with refactoring -remove NavigationPath from ODataIdContainer -make it easy for developer to create a NavigationPath from id/Model -Add model to APIHandlerFactory base class as a way to provide the model to the methods that require it -Experiment with setting the id during deserialization in cases where only key values are provided. Should we get this from ODL? -use the actual path for nested resources in readContext
    todo:
    -rationalize routing odatapath and uriparser odatapath usage
    -build out logic/add tests for the path in nested resources for readContext
    -should we get the id for key properties during deserialization from ODL?
    -handle edge cases throughout

--- src/Microsoft.AspNet.OData.Shared/ODataAPIHandlerFactory.cs
+++ src/Microsoft.AspNet.OData.Shared/ODataAPIHandlerFactory.cs
@@ -5,6 +5,8 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using Microsoft.OData.Edm;
+
 namespace Microsoft.AspNet.OData
 {
     /// <summary>
@@ -13,10 +15,34 @@ namespace Microsoft.AspNet.OData
     internal abstract class ODataAPIHandlerFactory
     {
         /// <summary>
+        /// Creates an instance of an ODataAPIHandlerFactory with the given moel
+        /// </summary>
+        /// <param name="model">The IEdmModel for the API Handler Factory</param>
+        protected ODataAPIHandlerFactory(IEdmModel model)
+        {
+            Model = model;
+        }
+
+        /// <summary>
         /// Get the handler depending on navigationpath
         /// </summary>
         /// <param name="navigationPath">Navigation path corresponding to an odataid</param>
-        /// <returns></returns>
+        /// <returns>ODataAPIHandler for the specified navigation path</returns>
         public abstract IODataAPIHandler GetHandler(NavigationPath navigationPath);
+
+        /// <summary>
+        /// Get the handler based on the odataPath
+        /// </summary>
+        /// <param name="odataPath"></param>
+        /// <returns>ODataAPIHandler for the specified odata path</returns>
+        public IODataAPIHandler GetHandler(string odataPath)
+        {
+            return this.GetHandler(NavigationPath.GetNavigationPath(odataPath, this.Model));
+        }
+
+        /// <summary>
+        /// The IEdmModel for the Factory
+        /// </summary>
+        public IEdmModel Model { get; private set; }
     }
 }

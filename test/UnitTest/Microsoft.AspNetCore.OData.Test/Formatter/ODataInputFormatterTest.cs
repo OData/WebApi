ea95dcf768a255c968ed9b8f0cc8b1c3855ac423
June Ngei <junengei@microsoft.com>
2022-05-31 09:17:31 +0300

    added tests to validate model state errors are added

---
+++ test/UnitTest/Microsoft.AspNetCore.OData.Test/Formatter/ODataInputFormatterTest.cs
@@ -0,0 +1,111 @@
+﻿#if NETCORE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Test.Abstraction;
+using Microsoft.AspNet.OData.Test.Builder.TestModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OData.Edm;
+using Xunit;
+#else
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Formatter;
+using Microsoft.AspNet.OData.Formatter.Serialization;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNet.OData.Routing.Conventions;
+using Microsoft.AspNet.OData.Test.Abstraction;
+using Microsoft.AspNet.OData.Test.Builder.TestModels;
+using Microsoft.AspNet.OData.Test.Common;
+using Microsoft.AspNet.OData.Test.Extensions;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+using Xunit;
+#endif
+
+namespace Microsoft.AspNet.OData.Test.Formatter
+{
+    public class ODataInputFormatterTest
+    {
+
+        [Fact]
+        public async Task ErrorsAddedToModelStateWhenILoggerServiceIsRegiatered()
+        {
+            // Arrange
+            const string requestbody = "{\"ID\":2,\"Name\":2}";
+
+            ODataConventionModelBuilder builder = ODataConventionModelBuilderFactory.Create();
+            builder.EntitySet<Customer>("Customers");
+            IEdmModel model = builder.GetEdmModel();
+            var controllers = new[] { typeof(CustomersController) };
+            var server = TestServerFactory.Create(controllers, (config) =>
+            {
+                config.MapODataServiceRoute("odata", null, model);
+            });
+
+            using (HttpClient client = TestServerFactory.CreateClient(server))
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "http://localhost/Customers"))
+            {
+                request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+                request.Content = new StringContent(requestbody);
+                
+                // Act
+                using (HttpResponseMessage response = await client.SendAsync(request))
+                {
+                    // Assert
+                    Assert.NotNull(response);
+                    Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+                }
+            }
+        }
+
+        public class Customer
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CustomersController : ODataController
+        {
+            private List<Customer> customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                ID = 1,
+                Name = "Jane",
+            }
+        };
+
+            [EnableQuery]
+            public IActionResult Post([FromBody] Customer customer)
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                customers.Add(customer);
+                return Ok(customers.LastOrDefault(c => c.ID == customer.ID));
+
+            }
+        }
+    }
+}

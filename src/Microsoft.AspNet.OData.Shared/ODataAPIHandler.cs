3965d6298af703bc4733a7691c91b319c291d7f4
Elizabeth Okerio <elizaokerio@gmail.com>
2022-07-13 11:25:29 +0300

    resolve resolvers and handlers (#2676)
    * resolve resolvers and handlers
    
    * update based on review comments

--- src/Microsoft.AspNet.OData.Shared/ODataAPIHandler.cs
+++ src/Microsoft.AspNet.OData.Shared/ODataAPIHandler.cs
@@ -5,7 +5,13 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
 
 namespace Microsoft.AspNet.OData
 {
@@ -50,5 +56,131 @@ namespace Microsoft.AspNet.OData
         /// <param name="navigationPropertyName">The name of the navigation property for the handler</param>
         /// <returns>The type of Nested ODataAPIHandler</returns>
         public abstract IODataAPIHandler GetNestedHandler(TStructuralType parent, string navigationPropertyName);
+
+        /// <summary>
+        /// The parent object
+        /// </summary>
+        internal TStructuralType ParentObject { get; set; }
+
+        /// <summary>
+        /// Apply OdataId for a resource with OdataID container
+        /// </summary>
+        /// <param name="resource">resource to apply odata id on</param>
+        /// <param name="model">The model.</param>
+        public virtual void UpdateLinkedObjects(TStructuralType resource, IEdmModel model)
+        {
+            if (resource != null && model != null)
+            {
+                this.ParentObject = resource;
+                CheckAndApplyODataId(resource, model);
+            }
+        }
+
+        private ODataPath GetODataPath(string path, IEdmModel model)
+        {
+            ODataUriParser parser = new ODataUriParser(model, new Uri(path, UriKind.Relative));
+            ODataPath odataPath = parser.ParsePath();
+
+            return odataPath;
+        }
+
+        private void CheckAndApplyODataId(object obj, IEdmModel model)
+        {
+            Type type = obj.GetType();
+            PropertyInfo property = type.GetProperties().FirstOrDefault(s => s.PropertyType == typeof(IODataIdContainer));
+            if (property != null && property.GetValue(obj) is IODataIdContainer container && container != null)
+            {
+                ODataPath odataPath = GetODataPath(container.ODataId, model);
+                object res = ApplyODataIdOnContainer(obj, odataPath);
+                foreach (PropertyInfo prop in type.GetProperties())
+                {
+                    object resVal = prop.GetValue(res);
+
+                    if (resVal != null)
+                    {
+                        prop.SetValue(obj, resVal);
+                    }
+                }
+            }
+            else
+            {
+                foreach (PropertyInfo prop in type.GetProperties().Where(p => !p.PropertyType.IsPrimitive))
+                {
+                    object propVal = prop.GetValue(obj);
+                    if (propVal == null)
+                    {
+                        continue;
+                    }
+
+                    if (propVal is IEnumerable lst)
+                    {
+                        foreach (object item in lst)
+                        {
+                            if (item.GetType().IsPrimitive)
+                            {
+                                break;
+                            }
+
+                            CheckAndApplyODataId(item, model);
+                        }
+                    }
+                    else
+                    {
+                        CheckAndApplyODataId(propVal, model);
+                    }
+                }
+            }
+        }
+
+        private object ApplyODataIdOnContainer(object obj, ODataPath odataPath)
+        {
+            KeySegment keySegment = odataPath.LastOrDefault() as KeySegment;
+            Dictionary<string, object> keys = keySegment?.Keys.ToDictionary(x => x.Key, x => x.Value);
+            if (keys != null)
+            {
+                TStructuralType returnedObject;
+                string error;
+                if (this.ParentObject.Equals(obj))
+                {
+                    if (this.TryGet(keys, out returnedObject, out error) == ODataAPIResponseStatus.Success)
+                    {
+                        return returnedObject;
+                    }
+                    else
+                    {
+                        if (this.TryCreate(keys, out returnedObject, out error) == ODataAPIResponseStatus.Success)
+                        {
+                            return returnedObject;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+                else
+                {
+                    IODataAPIHandler apiHandlerNested = this.GetNestedHandler(this.ParentObject, odataPath.LastSegment.Identifier);
+                    object[] getParams = new object[] { keys, null, null };
+                    if (apiHandlerNested.GetType().GetMethod(nameof(TryGet)).Invoke(apiHandlerNested, getParams).Equals(ODataAPIResponseStatus.Success))
+                    {
+                        return getParams[1];
+                    }
+                    else
+                    {
+                        if (apiHandlerNested.GetType().GetMethod(nameof(TryCreate)).Invoke(apiHandlerNested, getParams).Equals(ODataAPIResponseStatus.Success))
+                        {
+                            return getParams[1];
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }

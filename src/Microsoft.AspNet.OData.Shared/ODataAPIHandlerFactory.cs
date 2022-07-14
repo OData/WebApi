c1467cd1c6ca8ad8463ab08da2387d15f9a8b04b
Kennedy Kang'ethe <kemunga@microsoft.com>
2022-07-14 12:08:14 +0300

    Remove NavigationPath and PathItem (#2682)

--- src/Microsoft.AspNet.OData.Shared/ODataAPIHandlerFactory.cs
+++ src/Microsoft.AspNet.OData.Shared/ODataAPIHandlerFactory.cs
@@ -5,44 +5,49 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using System;
 using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
 
 namespace Microsoft.AspNet.OData
 {
     /// <summary>
-    /// Factory class for ODataAPIHandlers
+    /// Factory class for ODataAPIHandlers.
     /// </summary>
     public abstract class ODataAPIHandlerFactory
     {
         /// <summary>
-        /// Creates an instance of an ODataAPIHandlerFactory with the given moel
+        /// Creates an instance of an ODataAPIHandlerFactory with the given model.
         /// </summary>
-        /// <param name="model">The IEdmModel for the API Handler Factory</param>
+        /// <param name="model">The IEdmModel for the API Handler Factory.</param>
         protected ODataAPIHandlerFactory(IEdmModel model)
         {
             Model = model;
         }
 
         /// <summary>
-        /// Get the handler depending on navigationpath
-        /// </summary>
-        /// <param name="navigationPath">Navigation path corresponding to an odataid</param>
-        /// <returns>ODataAPIHandler for the specified navigation path</returns>
-        public abstract IODataAPIHandler GetHandler(NavigationPath navigationPath);
-
-        /// <summary>
-        /// Get the handler based on the odataPath
-        /// </summary>
-        /// <param name="odataPath"></param>
-        /// <returns>ODataAPIHandler for the specified odata path</returns>
-        public IODataAPIHandler GetHandler(string odataPath)
-        {
-            return this.GetHandler(new NavigationPath(odataPath, this.Model));
-        }
-
-        /// <summary>
-        /// The IEdmModel for the Factory
+        /// The IEdmModel for the Factory.
         /// </summary>
         public IEdmModel Model { get; private set; }
+
+        /// <summary>
+        /// Get the handler depending on OData path.
+        /// </summary>
+        /// <param name="odataPath">OData path corresponding to an odataid.</param>
+        /// <returns>ODataAPIHandler for the specified OData path.</returns>
+        public abstract IODataAPIHandler GetHandler(ODataPath odataPath);
+
+        /// <summary>
+        /// Get the handler based on the OData path uri string.
+        /// </summary>
+        /// <param name="path">OData path uri string.</param>
+        /// <returns>ODataAPIHandler for the specified odata path uri string.</returns>
+        public IODataAPIHandler GetHandler(string path)
+        {
+            ODataUriParser parser = new ODataUriParser(this.Model, new Uri(path, UriKind.Relative));
+            ODataPath odataPath = parser.ParsePath();
+
+            return this.GetHandler(odataPath);
+        }
     }
 }

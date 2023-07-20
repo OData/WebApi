beb8cb0f6a4df9ae0b1caf04cc8e8cb1a9474099
Kennedy Kang'ethe <kemunga@microsoft.com>
2023-07-20 14:52:42 +0300

    Null check in GenerateExpandQueryStringInternal (#2795)

--- src/Microsoft.AspNet.OData.Shared/Query/ExpandQueryBuilder.cs
+++ src/Microsoft.AspNet.OData.Shared/Query/ExpandQueryBuilder.cs
@@ -40,9 +40,17 @@ namespace Microsoft.AspNet.OData.Query
 
         private string GenerateExpandQueryStringInternal(object value, IEdmModel model, bool isNestedExpand)
         {
+            string expandString = "";
+
+            if (value == null || model == null)
+            {
+                return expandString;
+            }
+
             Type type = value.GetType();
             bool isCollection = TypeHelper.IsCollection(type, out Type elementType);
             IEnumerable collection = null;
+
             if (isCollection)
             {
                 type = elementType;
@@ -50,11 +58,15 @@ namespace Microsoft.AspNet.OData.Query
             }
 
             string edmFullName = type.EdmFullName();
-            IEdmSchemaType schemaType = model.FindType(edmFullName);
-            IEdmStructuredType edmStructuredType = schemaType as IEdmStructuredType;
+
+            IEdmStructuredType edmStructuredType = model.FindType(edmFullName) as IEdmStructuredType;
+
+            if (edmStructuredType == null)
+            {
+                return expandString;
+            }
 
             IEnumerable<IEdmNavigationProperty> navigationProperties = edmStructuredType.NavigationProperties();
-            string expandString = "";
 
             int count = 0;
 
